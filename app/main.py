a036dcaca876ee1cb49a75cb6a55d028e97e7da8
Taras Nester <tarasnester414@gmail.com>
2022-06-07 14:30:43 +0300

    base version

--- app/main.py
+++ app/main.py
@@ -1,3 +1,53 @@
+import json
+
+from app.customers import Customer
+from app.shops import Shop
+
+
+FILE_NAME = "../app/config.json"
+
+
 def shop_trip():
-    # write your code here
-    pass
+    shops, customers = create_shops_and_customers(FILE_NAME)
+
+    for customer in customers:
+        chosen_shop, trip_costs = get_customer_shop(customer, shops)
+        if chosen_shop is not None:
+            print(f"{customer.name} rides to {chosen_shop}\n")
+            chosen_shop.sell_products(customer)
+            print(f"{customer.name} rides home")
+            left_money = customer.pay_for_trip(trip_costs)
+            print(f'{customer.name} now has {left_money} dollars\n')
+        else:
+            print(f"{customer.name} doesn't have enough money to make purchase in any shop")
+
+
+def get_customer_shop(customer: Customer, shops: list[Shop]):
+    print(f"{customer.name} has {customer.money} dollars")
+
+    shops_cost = {}
+    for shop in shops:
+        costs = customer.trip_costs(shop)
+        print(f"{customer.name}'s trip to the {shop.name} costs {costs}")
+        shops_cost[costs] = shop
+    shops_cost = dict(sorted(shops_cost.items()))
+
+    for costs, shop in shops_cost.items():
+        if costs <= customer.money:
+            return shop, costs
+    return None, None
+
+
+def create_shops_and_customers(file_name: str):
+    with open(file_name) as file:
+        config_data = json.load(file)
+
+    fuel_price = config_data["FUEL_PRICE"]
+    shops = [Shop(item) for item in config_data["shops"]]
+    customers = [Customer(item, fuel_price) for item in config_data["customers"]]
+
+    return shops, customers
+
+
+if __name__ == '__main__':
+    shop_trip()
---
+++ app/shops.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+class Shop:
+    def __init__(self, shop_info):
+        self.name = shop_info["name"]
+        self.location = shop_info["location"]
+        self.products = shop_info["products"]
+
+    def __repr__(self):
+        return self.name
+
+    def sell_products(self, customer):
+        bill_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        products = customer.product_cart
+        bill = 0
+
+        print(f"Date: {bill_time}")
+        print(f"Thanks, {customer.name}, for you purchase!")
+        print("You have bought: ")
+        for product, amount in products.items():
+            products_cost = self.products[product] * amount
+            bill += products_cost
+            print(f"{amount} {product}s for {products_cost} dollars")
+        print(f"Total cost is {bill} dollars")
+        print("See you again!\n")
