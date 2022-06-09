e93641f1853c05772d3420bbedef76f68953f6a3
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-06-09 14:53:59 +0300

    split_shop_trip

--- app/main.py
+++ app/main.py
@@ -5,6 +5,21 @@ from app.shop import Shop
 
 
 def shop_trip():
+    fuel_price, customers, shops = create_customer_and_shop()
+
+    for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
+        cheapest_trip = customer.find_cheapest_trip_to_shop(shops, fuel_price)
+        if customer.money < cheapest_trip["cost"]:
+            print(f"{customer.name} doesn't have enough money "
+                  f"to make purchase in any shop")
+        else:
+            print(f"{customer.name} rides to {cheapest_trip['shop'].name}\n")
+            customer.ride_to_shop(cheapest_trip)
+            print(f"{customer.name} now has {customer.money} dollars\n")
+
+
+def create_customer_and_shop():
     with open("app/config.json", "r") as file:
         data = json.load(file)
     fuel_price: float = data["FUEL_PRICE"]
@@ -21,13 +36,8 @@ def shop_trip():
         shop = Shop(name, location, products_cost)
         shops[index] = shop
 
-    for customer in customers:
-        print(f"{customer.name} has {customer.money} dollars")
-        cheapest_trip = customer.find_cheapest_trip_to_shop(shops, fuel_price)
-        if customer.money < cheapest_trip["cost"]:
-            print(f"{customer.name} doesn't have enough money "
-                  f"to make purchase in any shop")
-        else:
-            print(f"{customer.name} rides to {cheapest_trip['shop'].name}\n")
-            customer.ride_to_shop(cheapest_trip)
-            print(f"{customer.name} now has {customer.money} dollars\n")
+    return fuel_price, customers, shops
+
+
+if __name__ == '__main__':
+    shop_trip()
