34f3de01f5220d39031d6baffe9729ab44030a60
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-09 17:46:06 +0300

    Solution

--- app/shop.py
+++ app/shop.py
@@ -1,6 +1,8 @@
+
+
 class Shop:
 
-    def __init__(self, name: str, location: list[int], products: dict):
+    def __init__(self, name: str, location: list, products: dict):
         self.name = name
         self.location = location
         self.products = products
