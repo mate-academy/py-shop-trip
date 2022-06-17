047428c7d7a60df0875635ecd7b98cd7568db109
Oleksii Isaiev <isaev.ieskela@gmail.com>
2022-06-17 17:03:59 +0300

    'Refactored'

--- app/main.py
+++ app/main.py
@@ -1,101 +1,77 @@
 import json
-
+import math
 import datetime
-
-from math import sqrt
 
 
 def shop_trip():
+    with open("app/config.json", "r") as data_json:
+        user_data = json.load(data_json)
 
-    with open("app/config.json") as data_file:
-        users_data = json.load(data_file)
+    fuel_price_data = user_data["FUEL_PRICE"]
+    customers_data = user_data["customers"]
+    shops_data = user_data["shops"]
 
-    fuel_price = users_data["FUEL_PRICE"]
-    customers = users_data["customers"]
-    shops = users_data["shops"]
-
-    for customer in customers:
+    for customer in customers_data:
         print(f"{customer['name']} has {customer['money']} dollars")
-        overall_prices = []
+        overall_prices = price_for_trip(customer, shops_data, fuel_price_data)
 
-        for shop in shops:
-            trip_coast = trip_to_shop(customer, shop, fuel_price)
-            print(f"{customer['name']}'s trip to the "
-                  f"{shop['name']} costs {trip_coast[0]}")
+        for shop, overall_price in overall_prices.items():
+            print(f"{customer['name']}'s trip to the {shop} "
+                  f"costs {round(overall_price, 2)}")
+        cheapest_shop = min(overall_prices, key=overall_prices.get)
 
-            overall_prices.append(trip_coast)
+        if customer["money"] >= overall_prices[cheapest_shop]:
+            print(f"{customer['name']} rides to {cheapest_shop}\n")
 
-        min_price, chosen_shop = min(overall_prices)
+            initial_location = customer["location"]
 
-        if customer['money'] > min_price:
-            print(f"{customer['name']} rides to {chosen_shop['name']}\n")
-            home = customer['location']
-            customer['location'] = chosen_shop["location"]
-            shop_prints_purchase_receipt(customer, chosen_shop)
-            print(f"{customer['name']} rides home")
-            customer['location'] = home
-            left_money = customer['money'] - min_price
-            print(f"{customer['name']} now has {left_money} dollars")
+            for shop in shops_data:
+                if shop["name"] == cheapest_shop:
+                    customer["location"] = shop["location"]
+                    print_invoice(customer, shop)
+
+            customer["location"] = initial_location
+            cash = customer["money"] - overall_prices[cheapest_shop]
+            print(f"{customer['name']} rides home\n"
+                  f"{customer['name']} now has {round(cash, 2)} dollars\n")
 
         else:
             print(f"{customer['name']} doesn't have "
-                  f"enough money to make purchase in any shop")
-
-        if int(customers.index(customer)) < len(customers) - 1:
-            print()
+                  "enough money to make purchase in any shop")
 
 
-def trip_to_shop(customer, shop, fuel_price):
+def price_for_fuel(customer, shop, fuel_price):
+    fuel_consumption = customer["car"]["fuel_consumption"] / 100
+    distance = math.dist(customer["location"], shop["location"])
+    fuel_cost = fuel_price * fuel_consumption * (distance * 2)
 
-    x_customer, y_customer, = customer["location"]
-    x_shop, y_shop, = shop["location"]
-    expenditure = customer["car"]["fuel_consumption"]
-
-    x_difference = x_customer - x_shop
-    y_difference = y_customer - y_shop
-
-    distance = round(sqrt(x_difference ** 2 + y_difference ** 2), 2)
-    fuel_cost = round((fuel_price * expenditure) / 100, 2)
-    cost_for_road = round(distance * fuel_cost, 2)
-
-    price_for_products = 0
-
-    for product_buy in customer["product_cart"]:
-        for product_sell in shop["products"]:
-            if product_buy == product_sell:
-                price_for_products += customer["product_cart"][product_buy] * \
-                    shop["products"][product_sell]
-
-    trip_coast = round(cost_for_road * 2 + price_for_products, 2)
-
-    return [trip_coast, shop]
+    return fuel_cost
 
 
-def shop_prints_purchase_receipt(customer, shop):
+def price_for_trip(customer, shops, fuel_price):
+    price_trip = {}
+    for shop in shops:
+        price_trip[shop["name"]] = price_for_fuel(customer, shop, fuel_price)
+        for product, amount in customer["product_cart"].items():
+            price_trip[shop["name"]] += shop["products"][product] * amount
 
-    now_date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    return price_trip
 
-    print(f"Date: {now_date}")
+
+def print_invoice(customer, shop):
+    invoice = 0
+    print(datetime.datetime.now().strftime("Date: %d/%m/%Y %H:%M:%S"))
     print(f"Thanks, {customer['name']}, for you purchase!")
     print("You have bought: ")
+    for quantity, price in customer["product_cart"].items():
+        purchase = shop["products"][quantity] * price
+        print(f"{price} {quantity}s for {purchase} dollars")
+        invoice += purchase
 
-    cost_of_products = 0
-
-    for product_buy in customer["product_cart"]:
-        for product_sell in shop["products"]:
-            if product_buy == product_sell:
-                count = customer['product_cart'][product_buy]
-                coast = customer['product_cart'][product_buy] * \
-                    shop["products"][product_sell]
-                print(f"{count} {product_buy}s for {coast} dollars")
-                cost_of_products += customer["product_cart"][product_buy] * \
-                    shop["products"][product_sell]
-
-    print(f"Total cost is {cost_of_products} dollars")
-    print("See you again!")
-
-    return cost_of_products
+    print(f"Total cost is {invoice} dollars\n"
+          "See you again!")
+    print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     shop_trip()
