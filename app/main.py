d05e2af20e8d9ba96c2692f9ec8489db1986bcdf
AnDy <ansmbox@gmail.com>
2022-08-25 16:49:53 +0300

    use "print()" instead of "yield"

--- app/main.py
+++ app/main.py
@@ -12,32 +12,32 @@ def go_shopping(
         fuel_price: float) -> str:
     trips = [Trip(customer, shop, fuel_price) for shop in shops]
 
-    yield f"{customer.name} has {customer.money} dollars"
+    print(f"{customer.name} has {customer.money} dollars")
 
     for trip in trips:
-        yield f"{customer.name}'s trip to the {trip.shop.name} "\
-              f"costs {trip.cost_of_trip}"
+        print(f"{customer.name}'s trip to the {trip.shop.name} "
+              f"costs {trip.cost_of_trip}")
 
     trip = min(trips, key=lambda x: x.cost_of_trip)
 
     if customer.money >= trip.cost_of_trip:
-        yield f"{customer.name} rides to {trip.shop.name}"
-        yield ""
-        yield "Date: 04/01/2021 12:33:41"
-        yield f"Thanks, {customer.name}, for you purchase!"
-        yield "You have bought: "
+        print(f"{customer.name} rides to {trip.shop.name}")
+        print("")
+        print("Date: 04/01/2021 12:33:41")
+        print(f"Thanks, {customer.name}, for you purchase!")
+        print("You have bought: ")
 
         for key, value in trip.cash_bill.items():
-            yield f"{key} for {value} dollars"
+            print(f"{key} for {value} dollars")
 
-        yield f"Total cost is {trip.cost_of_cart} dollars"
-        yield "See you again!"
-        yield ""
-        yield f"{customer.name} rides home"
-        yield f"{customer.name} now has {trip.cash_rest} dollars"
+        print(f"Total cost is {trip.cost_of_cart} dollars")
+        print("See you again!")
+        print("")
+        print(f"{customer.name} rides home")
+        print(f"{customer.name} now has {trip.cash_rest} dollars")
     else:
-        yield f"{customer.name} doesn't have enough money "\
-              "to make purchase in any shop"
+        print(f"{customer.name} doesn't have enough money "
+              "to make purchase in any shop")
 
 
 def shop_trip():
@@ -48,9 +48,8 @@ def shop_trip():
     customers = [Customer(customer) for customer in source_data["customers"]]
     shops = [Shop(shop) for shop in source_data["shops"]]
 
-    log = []
-
     for customer in customers:
-        log.append("\n".join(go_shopping(customer, shops, fuel_price)))
+        if customer != customers[0]:
+            print("")
 
-    print("\n\n".join(log))
+        go_shopping(customer, shops, fuel_price)
