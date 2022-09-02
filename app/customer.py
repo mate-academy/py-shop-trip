80ff8b436bd80c5d81134a8a8640907910f11e3f
Inna Kushnir <innakushnir63@gmail.com>
2022-09-02 11:02:32 +0300

    SOlo

--- app/customer.py
+++ app/customer.py
@@ -13,9 +13,8 @@ class Customer:
 
     def distance(self, other):
         distance = ((self.location[0] - other[0]
-                     ) ** 2 + (
-                self.location[1] - other[1]
-                               ) ** 2) ** 0.5
+                     ) ** 2 + (self.location[1] - other[1]) ** 2
+                    ) ** 0.5
         return distance
 
     def remainder_money(self, other):
