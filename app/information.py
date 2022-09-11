5b0ab6d73608731286264c90b1f13f2b822cd426
Volodymyr Panov <vpanov25@gmail.com>
2022-09-11 21:18:45 +0200

    Solution

--- app/information.py
+++ app/information.py
@@ -2,6 +2,6 @@ import json
 
 
 def customers_information():
-    with open("C:\\Users\\vpano\\py-shop-trip\\app\\config.json") as file:
+    with open("app/config.json") as file:
         information = json.load(file)
         return information
