83713e53c6d7dbc18d3d8af6f4ff07300917a782
Alex Parkhomenko <105493988+wQuelS@users.noreply.github.com>
2022-09-29 09:42:24 +0000

    fixed var names

--- app/shop.py
+++ app/shop.py
@@ -9,18 +9,18 @@ class Shop:
 
     def calculate_cost(self, product_cart: dict) -> int | None:
         total_cost = 0
-        for item in product_cart:
-            if item not in self.products:
+        for product in product_cart:
+            if product not in self.products:
                 return False
-            total_cost += product_cart[item] * self.products[item]
+            total_cost += product_cart[product] * self.products[product]
         return total_cost
 
     def print_purchase_receipt(self, name: str, product_cart: dict) -> None:
         print(datetime.datetime.now().strftime("Date: %d/%m/20%y %H:%M:%S"))
         print(f"Thanks, {name}, for you purchase!")
         print("You have bought: ")
-        for item in product_cart:
-            cost = product_cart[item] * self.products[item]
-            print(f"{product_cart[item]} {item}s for {cost} dollars")
+        for product in product_cart:
+            cost = product_cart[product] * self.products[product]
+            print(f"{product_cart[product]} {item}s for {cost} dollars")
         print(f"Total cost is {self.calculate_cost(product_cart)} dollars")
         print("See you again!\n")
