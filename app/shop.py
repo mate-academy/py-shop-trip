b99b9846ec31babc3033b08ed5320324693cdba8
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-10-05 14:07:15 +0300

    Solution for the task

---
+++ app/shop.py
@@ -0,0 +1,33 @@
+import datetime
+
+
+class Shop:
+    def __init__(self, shops: dict) -> None:
+        self.name = shops["name"]
+        self.location = shops["location"]
+        self.products = shops["products"]
+
+    def count_products_items(self, product_cart: dict) -> float:
+        price = 0
+
+        for key, value in product_cart.items():
+            price += value * self.products[key]
+
+        return price
+
+    def bill_for_shopping(self, name: str, product_cart: dict) -> None:
+        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Date: {date}")
+        print(f"Thanks, {name}, for you purchase!")
+
+        total_cost = 0
+
+        print("You have bought: ")
+
+        for product in self.products:
+            price = self.products[product] * product_cart[product]
+            total_cost += price
+            print(f"{product_cart[product]} {product}s for {price} dollars")
+
+        print(f"Total cost is {total_cost} dollars\n"
+              f"See you again!\n")
