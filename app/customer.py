26bc58550f092e1ae8240ece9a008b2a5570209a
AndriyKy <akrechovetskiy@gmail.com>
2022-10-06 18:57:48 +0300

    Solution

---
+++ app/customer.py
@@ -0,0 +1,35 @@
+from math import dist
+from typing import Union
+
+
+class Customer:
+    def __init__(self, name: str, money: Union[int, float]) -> None:
+        self.name = name
+        self.money = money
+
+    def money_amount(self) -> None:
+        print(f"{self.name} has {self.money} dollars")
+
+    def distance_calculation(self, location: list, shops: list) -> list:
+        distance_to_shops = []
+        for shop in shops:
+            distance = dist(location, shop["location"])
+            distance_to_shops.append(dict([
+                ("store name", shop["name"]),
+                ("distance", distance)
+            ]))
+        return distance_to_shops
+
+    def can_make_a_trip(self, trip: dict) -> bool:
+        if self.money > trip["cost of the trip"]:
+            print(f"{self.name} rides to {trip['store name']}")
+            return True
+        else:
+            print(f"{self.name} doesn't have enough money to make purchase "
+                  f"in any shop")
+            return False
+
+    def homecoming(self, trip: dict) -> None:
+        print(f"{self.name} rides home")
+        print(f"{self.name} now has {self.money - trip['cost of the trip']} "
+              f"dollars", end="\n\n")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,35 @@
-def shop_trip():
-    # write your code here
-    pass
+from app.customer import Customer
+from app.shop import Shop
+from app.car import Car
+
+import json
+
+
+def shop_trip() -> None:
+    with open("app/config.json") as file:
+        content = json.load(file)
+
+        for customer in content["customers"]:
+            customer_instance = Customer(customer["name"], customer["money"])
+            shop_instance = Shop(customer["product_cart"], content["shops"])
+            car_instance = Car(
+                content["FUEL_PRICE"],
+                customer["car"]["fuel_consumption"],
+                customer["name"]
+            )
+            customer_instance.money_amount()
+            distance_to_shops = customer_instance.distance_calculation(
+                customer["location"], content["shops"])
+            cost_of_purchases = shop_instance.purchase_cost()
+            most_profitable_trip = car_instance.cost_of_the_trip(
+                distance_to_shops, cost_of_purchases)
+            can_make_a_trip = customer_instance.can_make_a_trip(
+                most_profitable_trip)
+
+            if can_make_a_trip:
+                shop_instance.purchase_receipt(
+                    customer["name"],
+                    customer["product_cart"],
+                    most_profitable_trip
+                )
+                customer_instance.homecoming(most_profitable_trip)
