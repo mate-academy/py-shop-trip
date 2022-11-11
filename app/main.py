17c5f6403a15ff2debd26aa4cf64398e1409a35c
Yulia Bozhieva <bozhieva93@gmail.com>
2022-11-11 15:53:37 -0400

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,22 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+from app.customer import Customer
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("app/config.json") as file:
+        full_dict = json.load(file)
+
+    for sh in full_dict["shops"]:
+        shop = Shop(sh)
+        shop.add_to_shop_list()
+    shop_list = Shop.shop_list
+
+    for one_customer in full_dict["customers"]:
+        buyer = Customer(one_customer)
+        print(f"{buyer.name} has {buyer.money} dollars")
+        buyer.find_best_shop(shop_list, full_dict["FUEL_PRICE"])
+
+
+if __name__ == "__main__":
+    shop_trip()
