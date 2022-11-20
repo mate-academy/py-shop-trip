373faa7b206963841c6ba09527e023bacdf41e81
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-20 19:40:18 +0100

    Change indentation

--- app/Shops.py
+++ app/Shops.py
@@ -11,19 +11,14 @@ class Shop(ShopTrip):
 
     def change_location(self) -> None:
         shop_location = self.shops_location()[ShopTrip.shop]
-
         self.customer_location = shop_location
 
     def purchase(self) -> None:
         for (name_product1,
              num_product) in self.customer_product_cart()[self.name].items():
-
             product_price = self.shops_price()[ShopTrip.shop][name_product1]
-
             price = product_price * num_product
-
             self.total_cost += price
-
             print(f"{num_product} {name_product1}s"
                   f" for {price} dollars")
 
@@ -40,6 +35,5 @@ class Shop(ShopTrip):
     def ride_home(self) -> None:
         remaining_money = round(ShopTrip.money
                                 - ShopTrip.price_trip, 2)
-
         print(f"{self.name} rides home\n"
               f"{self.name} now has {remaining_money} dollars\n")
