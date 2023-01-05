96cb5255146ee9570212e2992ceea889d2fe660a
Oleksandr Mamchur <irex1988@gmail.com>
2023-01-05 15:38:11 +0200

    'split_part_of_code'

--- app/main.py
+++ app/main.py
@@ -2,39 +2,23 @@ from app.customers import Customer
 from app.shops import Shop
 from app.info import get_customers
 from app.info import get_shops
-from app.info import get_fuel_price
+from app.trip_cost import trip_cost
 
 
-fuel_price = get_fuel_price()
 shops = Shop.from_dict(get_shops())
 customers = Customer.from_dict(get_customers())
+trip_prices = trip_cost()
 
 
 def shop_trip() -> None:
-    trip_cost = []
-    for i, customer in enumerate(customers):
-        trip = []
-        for i, shop in enumerate(shops):
-            summ = 0
-            x1 = customer.location[0]
-            x2 = shop.location[0]
-            y1 = customer.location[1]
-            y2 = shop.location[1]
-            dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-            for key in shop.products:
-                summ += customer.cart[key] * shop.products[key]
-            fuel_rate = customer.car["fuel_consumption"]
-            tr = summ + 2 * dist * fuel_price * fuel_rate / 100
-            trip.append(round(tr, 2))
-        trip_cost.append(trip)
     for i, customer in enumerate(customers):
         print(f"{customer.name} has {customer.money} dollars")
         for index, shop in enumerate(shops):
-            if min(trip_cost[i]) == trip_cost[i][index]:
+            if min(trip_prices[i]) == trip_prices[i][index]:
                 low = index
             print(f"{customer.name}'s trip to the"
-                  f" {shop.name} costs {trip_cost[i][index]}")
-        if customer.money < min(trip_cost[i]):
+                  f" {shop.name} costs {trip_prices[i][index]}")
+        if customer.money < min(trip_prices[i]):
             print(f"{customer.name}"
                   f" doesn't have enough money to make purchase in any shop")
         else:
@@ -52,4 +36,4 @@ def shop_trip() -> None:
             print("See you again!\n")
             print(f"{customer.name} rides home")
             print(f"{customer.name} now has"
-                  f" {customer.money - min(trip_cost[i])} dollars\n")
+                  f" {customer.money - min(trip_prices[i])} dollars\n")
