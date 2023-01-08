fa3eb045d36049b31d31118748bdfff7b955a506
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-01-08 16:18:59 +0200

    Shop trip

--- app/customer.py
+++ app/customer.py
@@ -1,5 +1,6 @@
 from app.shop import Shop
 from app.car import Car
+import datetime
 
 
 class Customer:
@@ -29,3 +30,33 @@ class Customer:
         total_coast += self.product_cart["bread"] * shop.products["bread"]
         total_coast += self.product_cart["butter"] * shop.products["butter"]
         return round(total_coast, 2)
+
+    def go_shopping(self, shop: Shop, coast_per_liter: Car) -> None:
+        customer_home = self.location.copy()
+        print(f"{self.name} rides to {shop.name}")
+        spend_money = self.coast_of_trip_to_shop(shop, coast_per_liter)
+        print()
+        self.location = shop.location
+        data_of_purchase =\
+            datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Date: {data_of_purchase}")
+        print(f"Thanks, {self.name}, for you purchase!")
+        print("You have bought: ")
+        total_coast_of_products = 0
+        for product in self.product_cart:
+            coast_of_product = self.product_cart[product] * \
+                shop.products[product]
+            print(
+                f"{self.product_cart[product]} {product}s for"
+                f" {coast_of_product} dollars"
+            )
+            total_coast_of_products += self.product_cart[product] * \
+                shop.products[product]
+        print(f"Total cost is {total_coast_of_products} dollars")
+        self.money -= spend_money
+        print("See you again!")
+        print()
+        print(f"{self.name} rides home")
+        self.location = customer_home
+        print(f"{self.name} now has {self.money} dollars")
+        print()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import json
 
-from app.customer import Customer
 from app.car import Car
+from app.customer import Customer
 from app.shop import Shop
 
 
@@ -15,7 +15,6 @@ def shop_trip() -> None:
         shops = [Shop(shop) for shop in data_from_file["shops"]]
 
         for customer in customers:
-            customer_home = customer.location.copy()
             print(f"{customer.name} has {customer.money} dollars")
             dict_shop_and_coast = {}
             for shop in shops:
@@ -27,39 +26,18 @@ def shop_trip() -> None:
                 print(f"{customer.name}'s trip to "
                       f"the {shop.name} costs {coast_to_shop}")
 
-            list_keys = [key for key in dict_shop_and_coast]
-            minimal_coast = list_keys[0]
-            for key in dict_shop_and_coast:
-                if key < minimal_coast:
-                    minimal_coast = key
+            list_of_trip_coasts = [key for key in dict_shop_and_coast]
+            minimal_coast = list_of_trip_coasts[0]
+            for coast in dict_shop_and_coast:
+                if coast < minimal_coast:
+                    minimal_coast = coast
 
             if customer.money >= minimal_coast:
-                print(f"{customer.name} rides to "
-                      f"{dict_shop_and_coast[minimal_coast].name}")
-                print()
-                customer.location = dict_shop_and_coast[minimal_coast].location
-                print("Date: 04/01/2021 12:33:41")
-                print(f"Thanks, {customer.name}, for you purchase!")
-                print("You have bought: ")
-                total_coast_of_products = 0
-                for product in customer.product_cart:
-                    coast_of_product = customer.product_cart[product] * \
-                        dict_shop_and_coast[minimal_coast].products[product]
-                    print(
-                        f"{customer.product_cart[product]} {product}s for"
-                        f" {coast_of_product} dollars"
-                    )
-                    total_coast_of_products += \
-                        customer.product_cart[product] * \
-                        dict_shop_and_coast[minimal_coast].products[product]
-                print(f"Total cost is {total_coast_of_products} dollars")
-                customer.money -= minimal_coast
-                print("See you again!")
-                print()
-                print(f"{customer.name} rides home")
-                customer.location = customer_home
-                print(f"{customer.name} now has {customer.money} dollars")
-                print()
+                customer.go_shopping(
+                    dict_shop_and_coast[minimal_coast],
+                    fuel_coast
+                )
+
             else:
                 print(f"{customer.name} doesn't have enough"
                       f" money to make purchase in any shop")
