f160ae4be699c481ebc5fb1b8a1a07a10d6f3acb
Tania Kharchuk <tkharchuk14@gmail.com>
2023-01-12 16:48:03 +0200

    Solution2

--- app/customer.py
+++ app/customer.py
@@ -20,46 +20,27 @@ class Customer:
         self.money = money
         self.car = car
 
-    def fuel_costs(
-            self,
-            shop: Shop,
-            fuel_price: float
-    ) -> float:
+    def fuel_costs(self, shop: Shop, fuel_price: float) -> float:
         distance = math.dist(self.location, shop.location)
         fuel_costs = 2 * (distance * self.car.fuel_consumption
                           / 100 * fuel_price)
         return round(fuel_costs, 2)
 
-    def milk_costs(
-            self,
-            shop: Shop
-    ) -> float:
+    def milk_costs(self, shop: Shop) -> float:
         return self.product_cart["milk"] * shop.products["milk"]
 
-    def bread_costs(
-            self,
-            shop: Shop
-    ) -> float:
+    def bread_costs(self, shop: Shop) -> float:
         return self.product_cart["bread"] * shop.products["bread"]
 
-    def butter_costs(
-            self,
-            shop: Shop
-    ) -> float:
+    def butter_costs(self, shop: Shop) -> float:
         return self.product_cart["butter"] * shop.products["butter"]
 
-    def products_costs(
-            self,
-            shop: Shop
-    ) -> float:
+    def products_costs(self, shop: Shop) -> float:
         all_costs = self.milk_costs(shop) \
             + self.butter_costs(shop) + self.bread_costs(shop)
         return all_costs
 
-    def shop_prints(
-            self,
-            shop: Shop
-    ) -> None:
+    def shop_prints(self, shop: Shop) -> None:
         print("Date: 04/01/2021 12:33:41")
         # print(f"Date: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
         print(f"Thanks, {self.name}, for you purchase!")
@@ -73,11 +54,7 @@ class Customer:
         print(f"Total cost is {self.products_costs(shop)} dollars")
         print("See you again!\n")
 
-    def money_after_shopping(
-            self,
-            shop: Shop,
-            fuel_price: float
-    ) -> None:
+    def money_after_shopping(self, shop: Shop, fuel_price: float) -> None:
         current_money = self.money - self.products_costs(shop) \
             - self.fuel_costs(shop, fuel_price)
         print(f"{self.name} now has {current_money} dollars\n")
--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-from app.data_from_json import list_of_customers, list_of_shops, fuel_price
+from app.data_from_json import get_fuel_price, get_customers, get_shops
 
 
 def shop_trip() -> None:
     options_of_trip_costs = {}
-    for customer in list_of_customers:
+    for customer in get_customers():
         print(f"{customer.name} has {customer.money} dollars")
-        for shop in list_of_shops:
-            trip_costs = customer.fuel_costs(shop, fuel_price) \
+        for shop in get_shops():
+            trip_costs = customer.fuel_costs(shop, get_fuel_price()) \
                 + customer.products_costs(shop)
             print(f"{customer.name}'s trip to the "
                   f"{shop.name} costs {trip_costs}")
@@ -19,4 +19,4 @@ def shop_trip() -> None:
         print(f"{customer.name} rides to {chosen_shop.name}\n")
         customer.shop_prints(chosen_shop)
         print(f"{customer.name} rides home")
-        customer.money_after_shopping(chosen_shop, fuel_price)
+        customer.money_after_shopping(chosen_shop, get_fuel_price())
