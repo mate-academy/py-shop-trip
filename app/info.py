2217ea3afffe08d609807cbcd3dd146b2f64b12b
Anna Kabatova <kabatova.anna@ukr.net>
2023-01-12 16:33:18 +0200

    4th try

--- app/info.py
+++ app/info.py
@@ -2,6 +2,6 @@ import json
 
 
 def get_information() -> dict:
-    with open(r"app\config.json") as file:
+    with open("app/config.json", "r") as file:
         info = json.load(file)
         return info
