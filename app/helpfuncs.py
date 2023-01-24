927f56747a4230a604edda0f7ee8cbad9cb2be37
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-01-24 13:33:33 +0100

    Attempt

---
+++ app/helpfuncs.py
@@ -0,0 +1,52 @@
+
+
+def trip_cost(
+        customer_loc: list,
+        shop_loc: list,
+        fuel_price: float,
+        fuel_consump: float
+) -> float:
+    x1, y1, x2, y2 = customer_loc + shop_loc
+    way = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    return round((way * fuel_price * (fuel_consump / 100)) * 2, 2)
+
+
+def shopping_cost(num_to_buy: dict, prod_price: dict) -> float:
+    costs = []
+    prod_pack = {}
+    for key in num_to_buy:
+        if key in prod_price:
+            result = num_to_buy[key] * prod_price[key]
+            prod_pack[key] = [num_to_buy[key], result]
+            costs.append(result)
+    return [round(sum(costs), 2), prod_pack]
+
+
+def shopping_start(
+        name: str,
+        budget: float,
+        receipt: float,
+        cost: float,
+        shop: str,
+        prod_pack: dict
+) -> None:
+    if budget > cost:
+        print(
+            f"{name} rides to {shop}\n\n"
+            "Date: 04/01/2021 12:33:41\n"
+            f"Thanks, {name}, for you purchase!\n"
+            "You have bought:\n"
+            f"{prod_pack['milk'][0]} milks"
+            f" for {prod_pack['milk'][1]} dollars\n"
+            f"{prod_pack['bread'][0]} breads"
+            f" for {prod_pack['bread'][1]} dollars\n"
+            f"{prod_pack['butter'][0]} butters"
+            f" for {prod_pack['butter'][1]} dollars\n"
+            f"Total cost is {receipt} dollars\n"
+            "See you again!\n\n"
+            f"{name} rides home\n"
+            f"{name} now has {budget - cost} dollars\n"
+        )
+    else:
+        print(f"{name} doesn't have enough money"
+              f" to make purchase in any shop\n")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+from app.classes import Customer, Car, Shop
+from app.helpfuncs import shopping_cost, trip_cost, shopping_start
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as config:
+        json_info = json.load(config)
+        customers = json_info["customers"]
+        shops = json_info["shops"]
+        fuel_price = json_info["FUEL_PRICE"]
+    for customer in customers:
+        shopper = Customer.create_class(customer)
+        car = Car.create_class(shopper.car)
+        print(f"{shopper.name} has {shopper.money} dollars\n")
+        expense = 0
+        best_shop = ""
+        prod_pack = {}
+        for shop in shops:
+            destination = Shop.create_class(shop)
+            trip = trip_cost(
+                shopper.location,
+                destination.location,
+                fuel_price,
+                car.fuel_consumption
+            )
+            shopping, pack = shopping_cost(
+                shopper.product_cart,
+                destination.products
+            )
+            total = trip + shopping
+            if expense == 0 or total < expense:
+                expense = total
+                best_shop = destination.name
+                prod_pack = pack
+                receipt = shopping
+            print(f"{shopper.name}'s trip"
+                  f" to the {destination.name} costs {total}\n")
+        shopping_start(
+            shopper.name,
+            shopper.money,
+            receipt,
+            expense,
+            best_shop,
+            prod_pack
+        )
