e6d2ee84b4756f7fb618c66162ac81a178b9e629
IhorPokr <pokroveckijigor@gmail.com>
2023-01-26 10:00:22 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,43 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+from app.car import Car
+from app.shop import Shop
+from app.customer import Customer
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as shop_trip_file:
+        source_dict = json.load(shop_trip_file)
+
+    fuel_price = source_dict["FUEL_PRICE"]
+    customers = [Customer(
+        name=customer["name"],
+        product_cart=customer["product_cart"],
+        location=customer["location"],
+        money=customer["money"],
+        car=customer["car"])
+        for customer in source_dict["customers"]]
+    shops = [Shop(
+        name=shop["name"],
+        location=shop["location"],
+        products=shop["products"])
+        for shop in source_dict["shops"]]
+
+    for customer in customers:
+        car = Car(
+            brand=customer.car["brand"],
+            fuel_consumption=customer.car["fuel_consumption"]
+        )
+        customer.money_info()
+
+        shop_dict = {}
+        for shop in shops:
+            total_expenses = car.total_expenses(customer, shop, fuel_price)
+            shop_dict[shop] = total_expenses
+            car.trip_info(customer, shop, fuel_price)
+
+        go_to = min(shop_dict, key=shop_dict.get)
+        if shop_dict[go_to] <= customer.money:
+            go_to.shopping(customer)
+            car.come_back_info(customer, go_to, fuel_price)
+        else:
+            customer.less_money_info()
