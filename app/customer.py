c40cf084e2eac5d723e1040261c5e07ac6a92305
IhorPokr <pokroveckijigor@gmail.com>
2023-02-05 12:27:15 +0100

    'Solution'

--- app/customer.py
+++ app/customer.py
@@ -1,21 +1,33 @@
 class Customer:
-    def __init__(
-            self,
-            name: str,
-            product_cart: dict,
-            location: list,
-            money: int,
-            car: dict
-    ) -> None:
-        self.name = name
-        self.product_cart = product_cart
-        self.location = location
-        self.money = money
-        self.car = car
+    def __init__(self, name: str, product_cart: dict, location: list, money: int, car: dict):
+        self._name = name
+        self._product_cart = product_cart
+        self._location = location
+        self._money = money
+        self._car = car
 
-    def money_info(self) -> None:
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def product_cart(self):
+        return self._product_cart
+
+    @property
+    def location(self):
+        return self._location
+
+    @property
+    def money(self):
+        return self._money
+
+    @property
+    def car(self):
+        return self._car
+
+    def money_info(self):
         print(f"{self.name} has {self.money} dollars")
 
-    def less_money_info(self) -> None:
-        print(f"{self.name} doesn't have enough money to make purchase "
-              f"in any shop")
+    def less_money_info(self):
+        print(f"{self.name} doesn't have enough money to make purchase in any shop")
