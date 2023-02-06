482b46bd44552a44564bb02fb29b1a5b39500364
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-02-06 11:53:54 +0200

    Last commit 2

--- app/customer.py
+++ app/customer.py
@@ -21,59 +21,65 @@ class Customers:
     def calc_trip(self, market_list: Shops):
         dict_distance = {}
         for market in market_list.list_of_shops:
-            distance_to_shops = self.distance_two_points(market.location, self.location)
-            dict_distance[market.name] = distance_to_shops
-        # min_distance = min(dict_distance.values())
-        final_dict = dict([min(dict_distance.items(), key=lambda k_v: k_v[1])])
-
+            dict_distance[market.name] = self.distance_two_points(market.location, self.location)
+        min_distance = min(dict_distance.values())
+        final_dict = {k: v for k, v in dict_distance.items() if v == min_distance}
+        return final_dict
 
     #
     #         name_of_nearest_shop = min(distance_to_shops, key=lambda unit: distance_to_shops[unit])
+
+
 if __name__ == "__main__":
-    customers = [
-        {
-            "name": "Bob",
-            "product_cart": {
-                "milk": 4,
-                "bread": 2,
-                "butter": 5
-            },
-            "location": [12, -2],
-            "money": 55,
-            "car": {
-                "brand": "Suzuki",
-                "fuel_consumption": 9.9
-            }
-        },
-        {
-            "name": "Alex",
-            "product_cart": {
-                "milk": 2,
-                "bread": 2,
-                "butter": 2
-            },
-            "location": [1, -2],
-            "money": 41,
-            "car": {
-                "brand": "BMW",
-                "fuel_consumption": 9.1
-            }
-        },
-        {
-            "name": "Monica",
-            "product_cart": {
-                "milk": 3,
-                "bread": 3,
-                "butter": 1
-            },
-            "location": [11, -2],
-            "money": 12,
-            "car": {
-                "brand": "Audi",
-                "fuel_consumption": 7.6
-            }
-        }
-    ]
-    for customer in customers:
-        person = Customers()
-    print(person.prod_cart)
+    # customers = [
+    #     {
+    #         "name": "Bob",
+    #         "product_cart": {
+    #             "milk": 4,
+    #             "bread": 2,
+    #             "butter": 5
+    #         },
+    #         "location": [12, -2],
+    #         "money": 55,
+    #         "car": {
+    #             "brand": "Suzuki",
+    #             "fuel_consumption": 9.9
+    #         }
+    #     },
+    #     {
+    #         "name": "Alex",
+    #         "product_cart": {
+    #             "milk": 2,
+    #             "bread": 2,
+    #             "butter": 2
+    #         },
+    #         "location": [1, -2],
+    #         "money": 41,
+    #         "car": {
+    #             "brand": "BMW",
+    #             "fuel_consumption": 9.1
+    #         }
+    #     },
+    #     {
+    #         "name": "Monica",
+    #         "product_cart": {
+    #             "milk": 3,
+    #             "bread": 3,
+    #             "butter": 1
+    #         },
+    #         "location": [11, -2],
+    #         "money": 12,
+    #         "car": {
+    #             "brand": "Audi",
+    #             "fuel_consumption": 7.6
+    #         }
+    #     }
+    # ]
+    with open("config.json", "r") as data_file:
+        config_file = json.load(data_file)
+
+        for shop in config_file["shops"]:
+            market = Shops(shop)
+        for customer in config_file["customers"]:
+            person = Customers(customer)
+            print(person.calc_trip(market))
--- app/shops.py
+++ app/shops.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 # from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
-from customer import Customers
+# from customer import Customers
 
 
 class Shops:
@@ -13,13 +13,13 @@ class Shops:
         self.product = shop_dict["products"]
         self.__class__.list_of_shops.append(self)
 
-    def get_total(self, customer: Customers) -> float:
+    def get_total(self, customer) -> float:
         total_cost = sum(
             [customer.prod_cart[key] * self.product[key]
              for key in customer.prod_cart])
         return round(total_cost, 2)
 
-    def print_receipt(self, customer: Customers) -> None:
+    def print_receipt(self, customer) -> None:
         print(
             f"Date: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n"
             f"Thanks, {customer.name}, for you purchase! \n"
@@ -60,8 +60,5 @@ if __name__ == "__main__":
                 "butter": 3.5
             }
         }]
-    for market in shops:
-        market = Shops(market)
-    print(market.name)
 
 
