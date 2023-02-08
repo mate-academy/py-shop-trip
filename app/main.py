53b1b7d88d2f28aa08ac5eef6af1f1e54f63894b
Andrii Skryl <a.skryl92@gmail.com>
2023-02-08 20:41:50 +0200

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-import datetime
 import json
 from app.car import Car
 from app.shop import Shop
 from app.customer import Customer
+from app.print import (
+    get_shopping_options, get_receipt, get_home, not_enough_money)
 
 
 def shop_trip() -> None:
@@ -38,32 +39,11 @@ def shop_trip() -> None:
             shop_dict[shop] = car.get_total_trip_cost(
                 customer, shop, fuel_price
             )
-            print(f"{customer.name}'s trip to the {shop.name} costs "
-                  f"{car.get_total_trip_cost(customer, shop, fuel_price)}")
+            get_shopping_options(car, customer, shop, fuel_price)
 
         best_shop = min(shop_dict, key=shop_dict.get)
         if customer.money >= shop_dict[best_shop]:
-            print(f"{customer.name} rides to {best_shop.name}")
-
-            current_date = datetime.datetime.now()
-            print(f"\nDate: {current_date.strftime('%d/%m/%Y %H:%M:%S')}\n"
-                  f"Thanks, {customer.name}, for you purchase!\n"
-                  f"You have bought: ")
-
-            for product in customer.product_cart:
-                number = customer.product_cart[product]
-                price = number * best_shop.products[product]
-                print(f"{number} {product}s for {price} dollars")
-            print(f"Total cost is "
-                  f"{best_shop.get_shopping_cost(customer)} dollars\n"
-                  f"See you again!\n")
-
-            total_cost = car.get_total_trip_cost(
-                customer, best_shop, fuel_price
-            )
-            rest_on_balance = customer.money - total_cost
-            print(f"{customer.name} rides home\n"
-                  f"{customer.name} now has {rest_on_balance} dollars\n")
+            get_receipt(customer, best_shop)
+            get_home(car, customer, best_shop, fuel_price)
         else:
-            print(f"{customer.name} doesn't have enough money "
-                  f"to make purchase in any shop")
+            not_enough_money(customer)
