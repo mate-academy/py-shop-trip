2b993c666501fd35b8c421f10a97a133c0bb8ba9
Viacheslav Shcherbak <misternictof@gmail.com>
2023-02-16 15:15:16 +0200

    Fix clear blank line

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ from app.customer import Customer
 from app.shop import Shop
 
 
-def calculating_best_shop():
-    pass
-
 def shop_trip() -> None:
     input_data = json.load(open("app/config.json", "r"))
 
