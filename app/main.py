02891ecd51b8d7ee3c6eebcd8249f25f6d289135
Yulia <Mala>
2023-03-01 10:06:33 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,102 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.customer import Customer
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("config.json", "r") as file:
+        data = json.load(file)
+    fuel_price = data["FUEL_PRICE"]
+    customers = data["customers"]
+    shops = [Shop(one_shop) for one_shop in data["shops"]]
+    for customer_dict in customers:
+        customer = Customer(customer_dict)
+        best_shop, overall_cost = customer.plan_shopping(shops, fuel_price)
+        if customer.money <= overall_cost:
+            print(f"{customer.name} doesn't have enough money"
+                  f" to make purchase in any shop")
+            break
+        customer.go_shopping(best_shop, fuel_price, overall_cost)
+        best_shop.print_receipt(customer.name, customer.product_cart)
+        customer.back_home()
+
+
+# my_data = {
+#     "FUEL_PRICE": 2.4,
+#     "customers": [
+#         {
+#             "name": "Bob",
+#             "product_cart": {
+#                 "milk": 4,
+#                 "bread": 2,
+#                 "butter": 5
+#             },
+#             "location": [12, -2],
+#             "money": 55,
+#             "car": {
+#                 "brand": "Suzuki",
+#                 "fuel_consumption": 9.9
+#             }
+#         },
+#         {
+#             "name": "Alex",
+#             "product_cart": {
+#                 "milk": 2,
+#                 "bread": 2,
+#                 "butter": 2
+#             },
+#             "location": [1, -2],
+#             "money": 41,
+#             "car": {
+#                 "brand": "BMW",
+#                 "fuel_consumption": 9.1
+#             }
+#         },
+#         {
+#             "name": "Monica",
+#             "product_cart": {
+#                 "milk": 3,
+#                 "bread": 3,
+#                 "butter": 1
+#             },
+#             "location": [11, -2],
+#             "money": 12,
+#             "car": {
+#                 "brand": "Audi",
+#                 "fuel_consumption": 7.6
+#             }
+#         }
+#     ],
+#     "shops": [
+#         {
+#             "name": "Outskirts Shop",
+#             "location": [10, -5],
+#             "products": {
+#                 "milk": 3,
+#                 "bread": 1,
+#                 "butter": 2.5
+#             }
+#         },
+#         {
+#             "name": "Shop '24/7'",
+#             "location": [4, 3],
+#             "products": {
+#                 "milk": 2,
+#                 "bread": 1.5,
+#                 "butter": 3.2
+#             }
+#         },
+#         {
+#             "name": "Central Shop",
+#             "location": [0, 0],
+#             "products": {
+#                 "milk": 3,
+#                 "bread": 2,
+#                 "butter": 3.5
+#             }
+#         }
+#     ]
+# }
+#
+# shop_trip(my_data)
---
+++ app/shop.py
@@ -0,0 +1,18 @@
+class Shop:
+    def __init__(self, data: dict) -> None:
+        self.name: str = data["name"]
+        self.location: list = data["location"]
+        self.products: dict = data["products"]
+
+    def print_receipt(self, name: str, products: dict) -> None:
+        # print(f"Date: {datetime.now().strftime('%d/%m/%y %H:%M:%S')}")
+        print("Date: 04/01/2021 12:33:41")
+        print(f"Thanks, {name}, for you purchase!\n"
+              f"You have bought: ")
+        total_cost = 0
+        for item, amount in products.items():
+            item_cost = self.products[item] * amount
+            total_cost += item_cost
+            print(f"{amount} {item}s for {item_cost} dollars")
+        print(f"Total cost is {total_cost} dollars\n"
+              f"See you again!\n")
