a0a2a6a8f66ad14333e2ca8ddb850f927bc80d40
Maksym_Sylantiev <maxym2s@yahoo.com>
2023-03-02 15:39:10 +0200

    Solution

---
+++ app/shop.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+class Shop:
+    def __init__(
+            self,
+            name: str,
+            location: list,
+            products: dict
+    ) -> None:
+        self.name = name
+        self.location = location
+        self.products = products
+
+    def get_check(self, customer: any) -> None:
+        data = datetime.datetime.now().strftime("%d/%m/20%y %H:%M:%S")
+
+        print(f"Date: {data}")
+        print(f"Thanks, {customer.name}, for you purchase!")
+        print("You have bought: ")
+
+        for product, amount in customer.product_cart.items():
+            price = amount * self.products[product]
+            print(f"{amount} {product}s for {price} dollars")
+
+        print(f"Total cost is {customer.product_price(self).get(self.name)}"
+              f" dollars")
+        print("See you again!")
