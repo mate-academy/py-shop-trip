f63d812374ca3bd10a98d9cfd10fa7d755d7925e
kuznetsb <kuznetsbog@gmail.com>
2023-03-06 20:07:35 +0200

    Context manager overloading fixed

--- app/main.py
+++ app/main.py
@@ -10,28 +10,28 @@ def shop_trip() -> None:
     shops = []
     with open("app/config.json") as file:
         data = json.load(file)
-        Car.fuel_price = data["FUEL_PRICE"]
-        for customer in data["customers"]:
-            customers.append(
-                Customer(
-                    customer["name"],
-                    customer["product_cart"],
-                    customer["location"],
-                    customer["money"],
-                    Car(
-                        customer["car"]["brand"],
-                        customer["car"]["fuel_consumption"]
-                    )
+    Car.fuel_price = data["FUEL_PRICE"]
+    for customer in data["customers"]:
+        customers.append(
+            Customer(
+                customer["name"],
+                customer["product_cart"],
+                customer["location"],
+                customer["money"],
+                Car(
+                    customer["car"]["brand"],
+                    customer["car"]["fuel_consumption"]
                 )
             )
-        for shop in data["shops"]:
-            shops.append(
-                Shop(
-                    shop["name"],
-                    shop["location"],
-                    shop["products"]
-                )
+        )
+    for shop in data["shops"]:
+        shops.append(
+            Shop(
+                shop["name"],
+                shop["location"],
+                shop["products"]
             )
+        )
 
     for customer in customers:
         customer.make_purchase(shops)
