d7a0a6af259bf8cd8ee57c7dd84819f559ba895e
Roman <parom95@ukr.net>
2023-04-01 00:21:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,32 @@
-def shop_trip():
-    # write your code here
-    pass
+from app.shop import Shop
+from app.customer import Customer
+import json
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as f:
+        config = json.load(f)
+
+    fuel_price = config["FUEL_PRICE"]
+
+    customers = [
+        Customer(
+            person["name"],
+            person["product_cart"],
+            person["location"],
+            person["money"],
+            person["car"]["fuel_consumption"]
+        )
+        for person in config["customers"]
+    ]
+    shops = [
+        Shop(
+            shop["name"],
+            shop["location"],
+            shop["products"]
+        )
+        for shop in config["shops"]
+    ]
+
+    for customer in customers:
+        customer.cheapest_trip(shops, fuel_price)
