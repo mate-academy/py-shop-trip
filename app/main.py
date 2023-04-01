5ce9c745db249acc3809dda4d4916f2e1115c94a
Andrew <6594071@gmail.com>
2023-04-01 10:45:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
-def shop_trip():
-    # write your code here
-    pass
+from app.dataset.customers import get_all_customers
+from app.expenses.message import expanses_report
+from app.dataset.shops import get_all_shops
+
+
+def shop_trip() -> None:
+    customers = get_all_customers()
+    shops = get_all_shops()
+    for customer in customers:
+        expanses_report(customer, shops)
+
+
+shop_trip()
