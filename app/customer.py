359299e691e317ceb1af4a4e13d53177bf587c36
Anastasia Martyniuk <anastasia.20.12.1995@gmail.com>
2023-04-10 21:27:13 +0300

    Solution

---
+++ app/customer.py
@@ -0,0 +1,20 @@
+from car import Car
+
+
+class Customer:
+    def __init__(
+            self,
+            name: str,
+            product_cart: dict,
+            location: [int],
+            money: int,
+            car: Car
+    ) -> None:
+        self.name = name
+        self.product_cart = product_cart
+        self.location = location
+        self.money = money
+        self.car = car
+
+    def __str__(self) -> str:
+        return self.name
