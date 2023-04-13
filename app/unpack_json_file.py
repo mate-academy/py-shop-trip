233c6905923e42409b0c0031c26f234ba63ae40e
Alexandr <olexandrzhezheria@gmail.com>
2023-04-13 22:49:58 +0300

    New solution

--- app/unpack_json_file.py
+++ app/unpack_json_file.py
@@ -2,12 +2,14 @@ import json
 from typing import Any
 
 
-def unpack(json_file: json, data_name: str) -> Any:
+def unpack(json_file: str, data_name: str) -> Any:
     with open(json_file, "r") as file:
         data = json.load(file)
         if data_name == "fuel_price":
             return data["FUEL_PRICE"]
-        if data_name == "customers":
+        elif data_name == "customers":
             return data["customers"]
-        if data_name == "shops":
+        elif data_name == "shops":
             return data["shops"]
+        else:
+            return None
