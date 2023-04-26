e949c3f835037139a92975142cf35bc9ccd18978
Mykola <nickkozlov90@gmail.com>
2023-04-26 23:29:58 +0300

    Solution

--- app/classes.py
+++ app/classes.py
@@ -1,18 +1,23 @@
 from __future__ import annotations
-import dataclasses
 import math
 from typing import Dict, List
 
 
-@dataclasses.dataclass
 class Customer:
-    name: str
-    product_cart: Dict
-    location: List[int]
-    money: int
-    car: Dict
-    enough_money: bool = False
-    home_location = None
+    def __init__(
+            self,
+            name: str,
+            product_cart: Dict,
+            location: List[int],
+            money: int, car: Dict
+    ) -> None:
+        self.name = name
+        self.product_cart = product_cart
+        self.location = tuple(location)
+        self.money = money
+        self.car = car
+        self.enough_money = False
+        self.home_location = tuple(self.location)
 
     def calculate_products_cost(self, products: dict) -> float:
         return sum(
@@ -20,50 +25,58 @@ class Customer:
             for product in self.product_cart
         )
 
-    def calculate_fuel_cost(self, other: Shop, fuel_price) -> float:
+    def calculate_fuel_cost(self, other: Shop, fuel_price: float) -> float:
         distance = math.sqrt(
             pow((other.location[0] - self.location[0]), 2)
             + pow((other.location[1] - self.location[1]), 2)
         )
         return fuel_price * self.car["fuel_consumption"] * distance / 100 * 2
 
-    def pick_cheapest_trip(self, shops: List[Shop], fuel_price: float) -> tuple:
+    def pick_cheapest_trip(
+            self,
+            shops: List[Shop],
+            fuel_price: float
+    ) -> tuple:
         trips_cost = dict()
         for shop in shops:
             products_cost = self.calculate_products_cost(shop.products)
             fuel_cost = self.calculate_fuel_cost(shop, fuel_price)
             trip_cost = round(products_cost + fuel_cost, 2)
             print(
-                f"{self.name}'s trip to the {shop.name} Shop costs {trip_cost}"
+                f"{self.name}'s trip to the {shop.name} costs {trip_cost}"
             )
             trips_cost.update({shop: trip_cost})
         target_shop = min(trips_cost, key=trips_cost.get)
         return target_shop, trips_cost[target_shop]
 
     def drives_to_shop(self, shop: Shop) -> None:
-        self.home_location = list(self.location)
-        print(f"{self.name} rides to {shop.name} Shop\n")
+        print(f"{self.name} rides to {shop.name}\n")
         self.location = shop.location
         shop.print_receipt(self)
 
     def drives_home(self, trip_cost: float) -> None:
-        print(f"{self.name} rides home.")
+        print(f"{self.name} rides home")
         self.location = self.home_location
         self.money -= trip_cost
         print(f"{self.name} now has {self.money} dollars\n")
 
 
-@dataclasses.dataclass
 class Shop:
-    name: str
-    location: List[int]
-    products: Dict
+    def __init__(
+        self,
+        name: str,
+        location: List[int],
+        products: Dict
+    ) -> None:
+        self.name = name
+        self.location = tuple(location)
+        self.products = products
 
     def print_receipt(self, customer: Customer) -> None:
         print(
             f"Date: 04/01/2021 12:33:41\n"
             f"Thanks, {customer.name}, for your purchase!\n"
-            f"You have bought: \n"
+            f"You have bought: "
         )
         total_cost = 0
         for product in customer.product_cart:
@@ -76,7 +89,7 @@ class Shop:
                 product_name = f"{product_name}s"
             if cost > 1:
                 currency = f"{currency}s"
-            print(f"{quantity} {product_name} for {cost} {currency}\n")
+            print(f"{quantity} {product_name} for {cost} {currency}")
         print(
             f"Total cost is {total_cost} dollars\n"
             f"See you again!\n"
--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.convertor import customers, shops, fuel_price
 def shop_trip() -> None:
     for customer in customers:
         print(f"{customer.name} has {customer.money} dollars")
-        target_shop, trip_to_shop_cost = customer.pick_cheapest_trip(shops, fuel_price)
-        if customer.money >= trip_to_shop_cost:
+        target_shop, trip_cost = customer.pick_cheapest_trip(shops, fuel_price)
+        if customer.money >= trip_cost:
             customer.drives_to_shop(target_shop)
-            customer.drives_home(trip_to_shop_cost)
+            customer.drives_home(trip_cost)
         else:
             print(f"{customer.name} doesn't have enough money to make a "
-                  f"purchase in any shop\n")
+                  f"purchase in any shop")
