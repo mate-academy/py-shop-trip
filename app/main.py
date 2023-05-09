9a8bac68cfbb3d540d1f29ef310c7b9585c90d1e
Dmytry <donchykd@gmail.com>
2023-05-09 16:46:55 +0300

    Replace dict ride_price with list ride_price

--- app/main.py
+++ app/main.py
@@ -31,37 +31,33 @@ def shop_trip() -> None:
 
         print(f"{person.name} has {person.money} dollars")
 
-        ride_price = {}
+        ride_price = []
+
         for place in shops:
             distance = 2 * person.distance(person.location, place.location)
             total_price = person.trip_cost(distance,
                                            fuel_price,
                                            place.products)
             if total_price <= person.money:
-                ride_price[place.name] = total_price
+                ride_price.append((place.name, total_price))
 
             print(f"{person.name}'s trip to the {place.name} "
                   f"costs {round(total_price, 2)}")
 
         if len(ride_price) != 0:
-            shop_price_tuple = [(key, value)
-                                for key, value in ride_price.items()]
-            sorted_price_tuple = sorted(shop_price_tuple, key=lambda x: x[1])
-            sorted_price_dict = {key: value
-                                 for key, value in sorted_price_tuple}
-            shop_to_ride = next(iter(sorted_price_dict))
+            sorted_price_list = sorted(ride_price, key=lambda x: x[1])
+            shop_to_ride = sorted_price_list[0][0]
 
             print(f"{person.name} rides to {shop_to_ride}\n")
 
             for shop in shops:
                 if shop.name == shop_to_ride:
                     shop.purchase(person.name, person.product_cart)
-                    person.money -= sorted_price_dict[shop_to_ride]
+                    person.money -= sorted_price_list[0][1]
 
             print(f"{person.name} rides home\n"
                   f"{person.name} now has {round(person.money, 2)} dollars\n")
 
         if len(ride_price) == 0:
-
             print(f"{person.name} doesn't have enough money "
                   f"to make purchase in any shop")
