80c4a362e58c38867321c7407797470f85d8fc12
Oleksandr <@gmail.com>
2023-05-14 15:37:44 +0300

    'Solution'

--- app/customer.py
+++ app/customer.py
@@ -24,20 +24,26 @@ class Customer:
     def current_balance(self) -> None:
         print(f"{self.name} has {self.money} dollars")
 
-    def calculate_distance(self,
-                           shop_detail: Shop) -> float:
+    def calculate_distance(
+            self,
+            shop_detail: Shop
+    ) -> float:
         distance = ((sqrt((shop_detail.location[0]
                            - self.location[0]) ** 2
                           + (shop_detail.location[1]
                              - self.location[1]) ** 2)))
         return distance
 
-    def calculate_gasoline(self,
-                           fuel_price: int,
-                           shop_detail: Shop) -> float:
-        gasoline = (self.calculate_distance(shop_detail)
-                    * (self.car["fuel_consumption"] / 100)
-                    * fuel_price)
+    def calculate_gasoline(
+            self,
+            fuel_price: int,
+            shop_detail: Shop
+    ) -> float:
+        gasoline = (
+            self.calculate_distance(shop_detail)
+            * (self.car["fuel_consumption"] / 100)
+            * fuel_price
+        )
         return gasoline
 
     def product_cost(self, list_of_shop: Shop) -> float:
@@ -47,15 +53,21 @@ class Customer:
         )
         return total_expanse
 
-    def find_min_cost_shop(self,
-                           fuel_price: int,
-                           list_of_shop: list) -> dict:
+    def find_min_cost_shop(
+            self,
+            fuel_price: int,
+            list_of_shop: list
+    ) -> dict:
         min_coast_shop = {}
         for shop_detail in list_of_shop:
-            total = round(self.calculate_gasoline(fuel_price, shop_detail)
-                          * 2 + self.product_cost(shop_detail), 2)
-            print(f"{self.name}'s trip to the "
-                  f"{shop_detail.name} costs {total}")
+            total = round(
+                self.calculate_gasoline(fuel_price, shop_detail)
+                * 2 + self.product_cost(shop_detail), 2
+            )
+            print(
+                f"{self.name}'s trip to the "
+                f"{shop_detail.name} costs {total}"
+            )
             min_coast_shop[total] = shop_detail.name
         return min_coast_shop
 
@@ -63,33 +75,43 @@ class Customer:
         for product, amount in self.product_cart.items():
             price = (amount * shop_detail[product])
             print(f"{amount} {product}s for {price} dollars")
-        print(f"Total cost is "
-              f"{self.sum_purchase_details(shop_detail)} dollars")
-        print("See you again!\n")
-        print(f"{self.name} rides home")
+
+        print(
+            f"Total cost is {self.sum_purchase_details(shop_detail)} dollars\n"
+            f"See you again!\n\n"
+            f"{self.name} rides home"
+        )
 
     def sum_purchase_details(self, shop_detail: dict) -> float:
         return sum(amount * shop_detail[product]
                    for product, amount in self.product_cart.items())
 
-    def final_result(self,
-                     fuel_price: float,
-                     list_of_shop: list) -> None:
+    def final_result(
+            self,
+            fuel_price: float,
+            list_of_shop: list
+    ) -> None:
         cheap_shop = self.find_min_cost_shop(fuel_price, list_of_shop)
         if min(cheap_shop.keys()) < self.money:
             print(f"{self.name} rides to "
                   f"{cheap_shop.get(min(cheap_shop.keys()))}\n")
             data = datetime.datetime.now().strftime("%d/%m/20%y %H:%M:%S")
-            print(f"Date: {data}\nThanks, {self.name}, "
-                  f"for your purchase!\nYou have bought: ")
+            print(
+                f"Date: {data}\nThanks, {self.name}, "
+                f"for your purchase!\nYou have bought: "
+            )
             for detail in list_of_shop:
                 if detail.name == cheap_shop.get(min(cheap_shop.keys())):
                     self.purchase_details(detail.products)
                     fuel = self.calculate_gasoline(fuel_price, detail)
                     sum_purchase = self.sum_purchase_details(detail.products)
-                    print(f"{self.name} now has "
-                          f"{round(self.money - sum_purchase - fuel * 2, 2)}"
-                          f" dollars\n")
+                    print(
+                        f"{self.name} now has "
+                        f"{round(self.money - sum_purchase - fuel * 2, 2)}"
+                        f" dollars\n"
+                    )
         else:
-            print(f"{self.name} doesn't have enough "
-                  f"money to make a purchase in any shop")
+            print(
+                f"{self.name} doesn't have enough "
+                f"money to make a purchase in any shop"
+            )
--- app/main.py
+++ app/main.py
@@ -8,16 +8,21 @@ def shop_trip() -> None:
     with open("app/config.json", "r") as read_file:
         data = json.load(read_file)
         fuel_price = data["FUEL_PRICE"]
-        list_of_shop = [Shop(shop["name"],
-                             shop["location"],
-                             shop["products"])
-                        for shop in data["shops"]]
+        list_of_shop = [
+            Shop(shop["name"],
+                 shop["location"],
+                 shop["products"]
+                 )
+            for shop in data["shops"]]
+
         for customer in data["customers"]:
-            new_customer = Customer(customer["name"],
-                                    customer["product_cart"],
-                                    customer["location"],
-                                    customer["money"],
-                                    customer["car"])
+            new_customer = Customer(
+                customer["name"],
+                customer["product_cart"],
+                customer["location"],
+                customer["money"],
+                customer["car"]
+            )
 
             new_customer.current_balance()
             new_customer.final_result(fuel_price, list_of_shop)
