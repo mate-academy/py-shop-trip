d63695b3336d7939d2f6215fe20d02a6b1932405
Munkoor <eee1lvlboss@gmail.com>
2023-05-21 21:49:38 +0300

    Solution

---
+++ app/calculate_price.py
@@ -0,0 +1,20 @@
+from app.CustomerShopFolder.customer import Customer
+from app.CustomerShopFolder.shop import Shop
+
+
+def calculate_distance(customer_location: list, shop_location: list) -> float:
+    return ((customer_location[0] - shop_location[0]) ** 2 + (
+            customer_location[1] - shop_location[1]) ** 2) ** 0.5
+
+
+def calculate_trip_price(customer: Customer, shop: Shop,
+                         fuel_price: float) -> float:
+    # full distance: from home to shop and back
+    dist = calculate_distance(customer.location, shop.location) * 2
+    dist_price = customer.car.fuel_consumption / 100 * dist * fuel_price
+    products_price = 0
+
+    for product, amount in customer.product_cart.items():
+        products_price += shop.products[product] * amount
+
+    return dist_price + products_price
--- app/main.py
+++ app/main.py
@@ -1,3 +1,71 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+from datetime import datetime
+
+from app.calculate_price import calculate_trip_price
+from app.CustomerShopFolder.customer import Car, Customer
+from app.CustomerShopFolder.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("config.json", "r") as f:
+        data = json.load(f)
+
+    fuel_price = data["FUEL_PRICE"]
+    customers = []
+    shops = []
+
+    for customer in data["customers"]:
+        car = customer["car"]
+        customers.append(Customer(
+            name=customer["name"],
+            product_cart=customer["product_cart"],
+            location=customer["location"],
+            money=customer["money"],
+            car=Car(
+                brand=car["brand"],
+                fuel_consumption=car["fuel_consumption"])))
+
+    for shop in data["shops"]:
+        shops.append(Shop(
+            name=shop["name"],
+            location=shop["location"],
+            products=shop["products"]))
+
+    for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
+
+        min_trip = [shops[0],
+                    calculate_trip_price(customer, shops[0], fuel_price)]
+        for shop in shops:
+            current_trip_price = calculate_trip_price(customer, shop,
+                                                      fuel_price)
+            print(
+                f"{customer.name}'s trip to the "
+                f"{shop.name} costs {round(current_trip_price, 2)}")
+
+            if current_trip_price < min_trip[1]:
+                min_trip = [shop, current_trip_price]
+
+        if customer.money < min_trip[1]:
+            print(f"{customer.name} doesn't"
+                  f"have enough money to make a purchase in any shop")
+            break
+
+        print(f"{customer.name} rides to {min_trip[0].name}\n")
+        print(f"Date: {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
+        print(f"Thanks, {customer.name}, for your purchase!")
+        print("You have bought: ")
+
+        total_cost = 0
+
+        for product, amount in customer.product_cart.items():
+            total_cost += amount * min_trip[0].products[product]
+            print(
+                f"{amount} {product}s for"
+                f" {amount * min_trip[0].products[product]} dollars")
+
+        print(f"Total cost is {total_cost} dollars")
+        print("See you again!\n")
+        print("Bob rides home")
+        print(f"{customer.name} now has "
+              f"{round(customer.money - min_trip[1], 2)} dollars\n")
