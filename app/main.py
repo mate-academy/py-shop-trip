2cea5c0417f84731b8179344cc991ff01f9599e8
Volodymyr Bohomol <8773927@gmail.com>
2023-05-31 11:46:46 +0300

    Had written `shop_trip` function with 4 modules containing classes with data and logic for successful shopping trips.

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.trip import Trip
 def shop_trip() -> None:
     trip = Trip()
     trip.get_trip_data(
-        r"C:\Users\Nikhto\projects\py-shop-trip\app\config.json"
+        r"config.json"
     )
     trip.go_trip()
 
