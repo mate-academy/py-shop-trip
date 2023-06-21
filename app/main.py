af13f110d1e6b2360836ed5a73cac5634e9eacc0
Anton Komarov <anton_komarov_qa@ukr.net>
2023-06-21 16:42:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,37 @@
-def shop_trip():
-    # write your code here
-    pass
+from __future__ import annotations
+
+import json
+
+from app.customer import Customer
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as file:
+        config_file = json.load(file)
+
+    customers = [Customer.from_dict(customer)
+                 for customer
+                 in config_file["customers"]]
+    shops = [Shop.from_dict(shop)
+             for shop
+             in config_file["shops"]]
+
+    fuel_price = config_file["FUEL_PRICE"]
+
+    for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
+        shop_to_go = customer.choose_shop(shops, fuel_price)
+        if shop_to_go:
+            print(f"{customer.name} rides to {shop_to_go.name}\n")
+            shop_to_go.print_check(customer.name, customer.product_cart)
+            customer.money -= customer.trip_to_shop_cost(
+                shop_to_go,
+                fuel_price
+            )
+            print(f"{customer.name} rides home")
+            print(f"{customer.name} now has {customer.money} dollars\n")
+        else:
+            print(f"{customer.name} doesn't have enough money "
+                  f"to make a purchase in any shop")
+            continue
---
+++ app/shop.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import datetime
+
+
+class Shop:
+    def __init__(self,
+                 name: str,
+                 location: list,
+                 products: dict) -> None:
+        self.name = name
+        self.location = location
+        self.products = products
+
+    @classmethod
+    def from_dict(cls, shop_info: dict) -> Shop:
+        return cls(shop_info["name"],
+                   shop_info["location"],
+                   shop_info["products"])
+
+    def print_check(self,
+                    customer_name: str,
+                    customer_product_cart: dict) -> None:
+        print(f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+        print(f"Thanks, {customer_name}, for your purchase!")
+        print("You have bought: ")
+        total_cost = 0
+        for key, value in customer_product_cart.items():
+            total_cost += round(self.products[key] * value, 2)
+            print(f"{value} {key}s for "
+                  f"{round(self.products[key] * value, 2)} dollars")
+        print(f"Total cost is {total_cost} dollars")
+        print("See you again!\n")
