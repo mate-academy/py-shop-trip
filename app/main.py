2b302760935836132f90c5191d671cfd1d2b859a
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-21 20:44:31 +0300

    My solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,36 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.shop import create_shop
+from app.customer import create_customer
+
+
+def shop_trip() -> None:
+    data = {}
+    for _ in range(3):
+        with open("D:/mate/py-shop-trip/app/config.json", "r") as data_file:
+            data.update(json.load(data_file))
+    fuel_price = data["FUEL_PRICE"]
+    shops = []
+    for shop in data["shops"]:
+        shops.append(create_shop(shop))
+
+    customers = []
+    for customer in data["customers"]:
+        customers.append(create_customer(customer))
+
+    for traveler in customers:
+        print(f"{traveler.name} has {traveler.money} dollars")
+        costs = {}
+        for shop in shops:
+            cost = traveler.trip_cost(shop, fuel_price)
+            print(f"{traveler.name}'s trip to the {shop.name} costs {cost}")
+            costs[cost] = shop
+        if min(costs) > traveler.money:
+            print(f"{traveler.name} doesn't have enough "
+                  "money to make a purchase in any shop")
+            continue
+        chousen = costs[min(costs)]
+        traveler.arrived_to_shop(chousen, min(costs))
+        print(f"{traveler.name} rides to {chousen.name}")
+        traveler.create_receipt(chousen)
+        traveler.remaining_money()
