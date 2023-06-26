88efa4d7a0c320147f8fe4403be7aa20e8114b47
Roman Fedorchuk <slasherroman@gmail.com>
2023-06-26 12:47:58 +0300

    version2

--- app/main.py
+++ app/main.py
@@ -5,16 +5,20 @@ from app.car import Car
 
 
 def shop_trip() -> None:
+    shops = {}
     with open("app/config.json", "r") as config:
         data = json.load(config)
         customers_data = data["customers"]
         shops_data = data["shops"]
         fuel_price = data["FUEL_PRICE"]
 
-    for one_customer in customers_data:
-        trip_prices = {}
-        shops = {}
+    for one_shop in shops_data:
+        shop = Shop(one_shop["name"],
+                    one_shop["location"],
+                    one_shop["products"])
+        shops[shop.name] = shop
 
+    for one_customer in customers_data:
         client = Customer(one_customer["name"],
                           one_customer["product_cart"],
                           one_customer["location"],
@@ -22,14 +26,5 @@ def shop_trip() -> None:
         print(f"{client.name} has {client.money} dollars")
         car = Car(one_customer["car"]["brand"],
                   one_customer["car"]["fuel_consumption"])
-        for one_shop in shops_data:
-            shop = Shop(one_shop["name"],
-                        one_shop["location"],
-                        one_shop["products"])
-            trip_prices[shop.name] = calculate_trip_price(
-                fuel_price,
-                client,
-                shop,
-                car)
-            shops[shop.name] = shop
+        trip_prices = calculate_trip_price(fuel_price, client, shops, car)
         check_best_price(trip_prices, client, shops)
--- app/shop.py
+++ app/shop.py
@@ -12,21 +12,28 @@ class Shop:
     products: dict
 
 
+def calculate_distance(client: Customer, shop: Shop) -> float:
+    return math.sqrt(
+        ((client.location[0] - shop.location[0]) ** 2)
+        + ((client.location[1] - shop.location[1]) ** 2))
+
+
 def calculate_trip_price(
         fuel_price: float,
         client: Customer,
-        shop: Shop,
-        car: Car) -> float:
-    distance = math.sqrt(
-        ((client.location[0] - shop.location[0]) ** 2)
-        + ((client.location[1] - shop.location[1]) ** 2))
-    trip_price = fuel_price * distance * (car.fuel_consumption / 100) * 2
-    for product, price in shop.products.items():
-        if product in client.products_cart:
-            trip_price += (price * client.products_cart.get(product))
-    trip_price = round(trip_price, 2)
-    print(f"{client.name}'s trip to the {shop.name} costs {trip_price}")
-    return trip_price
+        shops: dict,
+        car: Car) -> dict:
+    trip_prices = {}
+    for shop in shops.values():
+        trip_price = (fuel_price * calculate_distance(client, shop)
+                      * (car.fuel_consumption / 100) * 2)
+        for product, price in shop.products.items():
+            if product in client.products_cart:
+                trip_price += (price * client.products_cart.get(product))
+        trip_price = round(trip_price, 2)
+        print(f"{client.name}'s trip to the {shop.name} costs {trip_price}")
+        trip_prices[shop.name] = trip_price
+    return trip_prices
 
 
 def check_best_price(
@@ -34,18 +41,17 @@ def check_best_price(
         client: Customer,
         shops: dict) -> None:
     best_choice = min(trip_prices.values())
+    best_shop = min(trip_prices, key=trip_prices.get)
     rest = client.money - best_choice
     if best_choice > client.money:
         print(
             f"{client.name} doesn't have enough money "
             f"to make a purchase in any shop")
     else:
-        for best_shop, best_price in trip_prices.items():
-            if best_price == best_choice:
-                print(f"{client.name} rides to {best_shop}\n")
-                create_bill(client, shops, best_shop)
-                print(f"{client.name} rides home\n"
-                      f"{client.name} now has {rest} dollars\n")
+        print(f"{client.name} rides to {best_shop}\n")
+        create_bill(client, shops, best_shop)
+        print(f"{client.name} rides home\n"
+              f"{client.name} now has {rest} dollars\n")
 
 
 def create_bill(client: Customer, shops: dict, best_shop: str) -> None:
