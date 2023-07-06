b633ac6af4e4fd3fcd63640c09140115780d4e5e
Dmytro Kucherenko <dimakucherenko91@gmail.com>
2023-07-06 18:13:28 +0300

    'Solution'

---
+++ app/customer.py
@@ -0,0 +1,41 @@
+from math import sqrt
+from app.car import Car
+from app.shop import Shop
+
+
+class Customer:
+    def __init__(
+            self,
+            name: str,
+            location: list,
+            money: int,
+            product_cart: dict,
+            car: Car
+    ) -> None:
+        self.name = name
+        self.location = tuple(location)
+        self.money = money
+        self.product_cart = product_cart
+        self.car = car
+
+    def find_distance(self, shop_location: list) -> float:
+        x1, y1 = self.location
+        x2, y2 = shop_location
+
+        distance = sqrt((x2 - x1)**2 + (y2 - y1) ** 2)
+        return distance
+
+    def make_purchase(self, shop: Shop) -> None:
+
+        cost = 0
+        print("Date: 04/01/2021 12:33:41")
+        print(f"Thanks, {self.name}, for your purchase!")
+        print("You have bought:")
+        for item, quantity in self.product_cart.items():
+            if item in shop.products:
+                item_price = shop.products[item]
+                item_total_price = item_price * quantity
+                cost += item_total_price
+                print(f"{quantity} {item}s for {item_total_price} dollars")
+        print(f"Total cost is {cost} dollars")
+        print("See you again!")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,74 @@
+import os
+import json
+from app.shop import Shop
+from app.customer import Customer
+from app.car import Car
+
+
 def shop_trip():
-    # write your code here
-    pass
+
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "config.json"
+    full_path = os.path.join(absolute_path, relative_path)
+
+    with open(full_path) as config_file:
+        data = json.load(config_file)
+
+    fuel_price = data["FUEL_PRICE"]
+    customers_data = data["customers"]
+    shops_data = data["shops"]
+
+    customers = []
+    shops = []
+
+    for customer_data in customers_data:
+        name = customer_data["name"]
+        product_cart = customer_data["product_cart"]
+        location = customer_data["location"]
+        money = customer_data["money"]
+        car_data = customer_data["car"]
+        car = Car(car_data["brand"], car_data["fuel_consumption"])
+
+        customer = Customer(name, location, money, product_cart, car)
+        customers.append(customer)
+
+    for shop_data in shops_data:
+        name = shop_data["name"]
+        location = shop_data["location"]
+        products = shop_data["products"]
+
+        shop = Shop(name, location, products)
+        shops.append(shop)
+
+    for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
+        trip_costs = {}
+
+        for shop in shops:
+            distance = customer.find_distance(shop.shop_location)
+            fuel_cost = customer.car.calculate_price(distance, fuel_price)
+            product_cost = shop.calculate_product_cost(customer.product_cart)
+
+            total_cost = fuel_cost * 2 + product_cost
+            trip_costs[shop] = total_cost
+
+            print(f"{customer.name}'s trip to "
+                  f"{shop.shop_name} costs {round(total_cost, 2)}")
+
+        min_cost_shop = min(trip_costs, key=trip_costs.get)
+
+        if customer.money >= min(trip_costs.values()):
+            print(f"{customer.name} rides to {min_cost_shop.shop_name} \n")
+
+            customer.make_purchase(min_cost_shop)
+            print(f"\n{customer.name} rides home")
+            customer.money -= min(trip_costs.values())
+            print(f"{customer.name} now "
+                  f"has {round(customer.money, 2)} dollars\n")
+
+        else:
+            print(f"{customer.name} doesn't have "
+                  f"enough money to make a purchase in any shop")
+
+
+shop_trip()
