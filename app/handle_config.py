7416774c276cb8fb5d6f1606a5e88acfabb41415
Oleksii Kolii <igravkompik@gmail.com>
2023-07-10 12:50:26 +0300

    renamed vars

--- app/handle_config.py
+++ app/handle_config.py
@@ -8,6 +8,6 @@ from app.customer import customer_from_dict
 def handle_config() -> Any:
     with open("app/config.json") as file:
         config = json.load(file)
-    customers = [customer_from_dict(d) for d in config["customers"]]
-    shops = [shop_from_dict(d) for d in config["shops"]]
+    customers = [customer_from_dict(customer) for customer in config["customers"]]
+    shops = [shop_from_dict(shop) for shop in config["shops"]]
     return config["FUEL_PRICE"], customers, shops
