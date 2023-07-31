5c98ec88fe9aaa74656b224ddc6ecf5eddd13005
Vlad Kleshko <vkoesko@gmail.com>
2023-07-31 10:17:19 +0300

    for checking

---
+++ app/customer.py
@@ -0,0 +1,52 @@
+import datetime
+
+from math import sqrt
+from typing import List
+
+from app.car import Car
+from app.shop import Shop
+
+
+class Customer:
+    def __init__(
+            self,
+            name: str,
+            product_cart: dict,
+            customer_location: List[int],
+            money: int,
+            car: Car
+    ) -> None:
+        self.name = name
+        self.product_cart = product_cart
+        self.customer_location = customer_location
+        self.money = money
+        self.car = car
+
+    def initial_money(self) -> None:
+        print(f"{self.name} has {self.money} dollars")
+
+    def distance(self, shop_location: list, fuel_consumption: float, fuel_price: float) -> float:
+        x1, y1 = self.customer_location
+        x2, y2 = shop_location
+        distance_to_shop = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        trip_to_shop = distance_to_shop / 100 * fuel_consumption * fuel_price * 2
+
+        return round(trip_to_shop, 2)
+
+    def time_for_shopping(self, shop: Shop) -> None:
+        total_cost = 0.0
+        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Date: {date}")
+        print(f"Thanks, {self.name}, for your purchase!")
+        print("You have bought:")
+
+        product_price_list = [value for value in shop.products.values()]
+        product_list = iter(product_price_list)
+
+        for key, value in self.product_cart.items():
+            cost = value * next(product_list)
+            total_cost += cost
+            print(f"{value} {key}s for {cost} dollars")
+
+        print(f"Total cost is {total_cost} dollars")
+        print("See you again!")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,58 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.shop import Shop
+from app.car import Car
+from app.customer import Customer
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as data_file:
+        data = json.load(data_file)
+
+    fuel_price = data["FUEL_PRICE"]
+    customers_data = data["customers"]
+    shops_data = data["shops"]
+
+    for customer_data in customers_data:
+        customer_car = Car(customer_data["car"]["brand"], customer_data["car"]["fuel_consumption"])
+        customer = Customer(customer_data["name"], customer_data["product_cart"], customer_data["location"],
+                            customer_data["money"], customer_car)
+
+        min_total_cost = float('inf')
+        best_shop = None
+
+        customer.initial_money()
+
+        for shop_data in shops_data:
+            shop_name = shop_data["name"]
+            shop_location = shop_data["location"]
+            products = shop_data["products"]
+
+            shop = Shop(shop_name, shop_location, products)
+
+            total_cost = (customer.distance(shop.shop_location, customer_car.fuel_consumption, fuel_price))
+
+            product_price_list = [value for value in products.values()]
+            product_list = iter(product_price_list)
+            for key, value in customer.product_cart.items():
+                cost = value * next(product_list)
+                total_cost += cost
+
+            print(f"{customer.name}'s trip to the {shop_name} costs {round(total_cost, 2)}")
+
+            if total_cost < min_total_cost and total_cost <= customer.money:
+                min_total_cost = total_cost
+                best_shop = shop
+
+        if best_shop is not None:
+            print(f"{customer.name} rides to {best_shop.shop_name}\n")
+
+            customer.time_for_shopping(best_shop)
+            customer.money -= min_total_cost
+            print(f"{customer.name} now has {round(customer.money, 2)} dollars\n")
+
+        else:
+            print(f"{customer.name} doesn't have enough money to make a purchase in any shop\n")
+
+
+shop_trip()
