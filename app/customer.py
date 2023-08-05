39c60225333597ff3deaa5491b0163475c3e2194
Illia Zhmurskiy <thebrawler6@gmail.com>
2023-08-05 11:55:09 +0300

    Solution

--- app/customer.py
+++ app/customer.py
@@ -1,29 +1,54 @@
+from app.car import Car
+from app.shop import Shop
+
+
 class Customer:
-    def __init__(self, name, product_cart, location, money, car):
+    def __init__(
+            self,
+            name: str,
+            product_cart: dict,
+            location: list[int],
+            money: int,
+            car: dict
+    ) -> None:
         self.name = name
         self.product_cart = product_cart
         self.location = location
         self.money = money
-        self.car = car
+        self.car = Car(**car)
 
-    def calculate_trip_cost(self, shop, fuel_price):
-        distance_to_shop = self.calculate_distance(shop.location)
-        fuel_cost_to_shop = self.car.fuel_cost(distance_to_shop, fuel_price)
-        total_product_cost = self.calculate_product_cost(shop.products)
-        total_trip_cost = 2*fuel_cost_to_shop + total_product_cost
-        return round(total_trip_cost, 2)
+    def get_trip_price(
+            self,
+            shop: Shop,
+            fuel_price:
+            int | float
+    ) -> int | float:
+        distance = (
+            (
+                (shop.location[0] - self.location[0]) ** 2
+                + (shop.location[1] - self.location[1]) ** 2
+            ) ** 0.5
+        )
+        fuel_cost = fuel_price * (
+            self.car.fuel_consumption / 100
+        ) * distance * 2
 
-    def calculate_distance(self, destination_location):
+        products_price = sum(
+            [
+                shop.products[product] * amount
+                for product, amount in self.product_cart.items()
+            ]
+        )
+        return fuel_cost + products_price
+
+    def calculate_distance(self, destination_location: list[int]) -> float:
         x1, y1 = self.location
         x2, y2 = destination_location
         return round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5, 2)
 
-    def calculate_product_cost(self, shop_products):
+    def calculate_product_cost(self, shop_products: dict) -> float | int:
         total_cost = 0
         for product, quantity in self.product_cart.items():
             if product in shop_products:
                 total_cost += shop_products[product] * quantity
         return round(total_cost, 2)
-
-    def update_money(self, amount):
-        self.money -= amount
--- app/main.py
+++ app/main.py
@@ -1,57 +1,63 @@
+import datetime
 import json
-from app.car import Car
+
 from app.customer import Customer
 from app.shop import Shop
-from datetime import datetime
 
 
-def shop_trip():
+def shop_trip() -> None:
     with open("app/config.json") as config_file:
-    # with open("config.json") as config_file:
         config_data = json.load(config_file)
 
     fuel_price = config_data["FUEL_PRICE"]
     customers_data = config_data["customers"]
     shops_data = config_data["shops"]
 
-    shops = [Shop(shop["name"], shop["location"], shop["products"]) for shop in shops_data]
+    shops: list[Shop] = [Shop(**shop) for shop in shops_data]
+    customers: list[Customer] = [Customer(**customer)
+                                 for customer in customers_data]
 
-    for customer_data in customers_data:
-        customer_name = customer_data["name"]
-        product_cart = customer_data["product_cart"]
-        location = customer_data["location"]
-        money = customer_data["money"]
-        car_data = customer_data["car"]
-        car_brand = car_data["brand"]
-        fuel_consumption = car_data["fuel_consumption"]
+    for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
 
-        car = Car(car_brand, fuel_consumption)
-        customer = Customer(customer_name, product_cart, location, money, car)
-
-        cheapest_shop = None
         cheapest_trip_cost = float("inf")
+        cheapest_shop = shops[0]
 
         for shop in shops:
-            trip_cost = customer.calculate_trip_cost(shop, fuel_price)
-            if trip_cost < cheapest_trip_cost and trip_cost <= customer.money:
+            trip_cost = customer.get_trip_price(shop, fuel_price)
+            current_shop = shop
+            current_trip_cost = trip_cost
+
+            if trip_cost < cheapest_trip_cost:
                 cheapest_shop = shop
                 cheapest_trip_cost = trip_cost
+            if (
+                    current_shop == shops[-1]
+                    and customer.money < cheapest_trip_cost
+            ):
+                print(f"{customer.name}'s "
+                      f"trip to the {current_shop.name}"
+                      f" costs {current_trip_cost:.2f}", end="")
+            else:
+                print(f"{customer.name}'s"
+                      f" trip to the {current_shop.name} "
+                      f"costs {current_trip_cost:.2f}")
 
-        if cheapest_shop:
-            print(f"{customer.name} has {customer.money} dollars")
-            print(f"{customer.name}'s trip to {cheapest_shop.name} costs {cheapest_trip_cost:.2f}")
-            customer.update_money(cheapest_trip_cost)
+        if customer.money < cheapest_trip_cost:
+            print(f"\n{customer.name}"
+                  f" doesn't have enough money to make a purchase in any shop")
+        else:
+            customer.money -= cheapest_trip_cost
             print(f"{customer.name} rides to {cheapest_shop.name}")
 
-            purchase_time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            purchase_time = (datetime.datetime.now().strftime
+                             ("%d/%m/%Y %H:%M:%S"))
             cheapest_shop.print_receipt(customer, purchase_time)
 
             customer.location = cheapest_shop.location
 
             print(f"\n{customer.name} rides home")
-            print(f"{customer.name} now has {customer.money:.2f} dollars")
-        else:
-            print(f"\n{customer.name} doesn't have enough money to make a purchase in any shop")
+            print(f"{customer.name} now has {customer.money:.2f} dollars\n")
 
 
 if __name__ == "__main__":
