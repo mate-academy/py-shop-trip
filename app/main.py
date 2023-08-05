e5c447903a4797751d01cd01e3700e917cda173b
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-08-05 18:40:38 +0300

    create functions

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.shop import Shop
 import json
 
 
-def shop_trip() -> None:
+def load_config() -> tuple[float, list[Customer], list[Shop]]:
     with open("app/config.json") as file:
         config = json.load(file)
 
@@ -25,33 +25,39 @@ def shop_trip() -> None:
              products=shop["products"])
         for shop in config["shops"]
     ]
+
+    return fuel_price, customers, shops
+
+
+def print_purchase_receipt(customer: Customer, shop: Shop) -> None:
+    customer.print_the_purchase_receipt(shop)
+    print(f"Total cost is {customer.get_product_price(shop)} dollars")
+    print("See you again!\n")
+
+
+def shop_trip() -> None:
+    fuel_price, customers, shops = load_config()
+
     for customer in customers:
+        print(f"{customer.name} has {customer.money} dollars")
         cheapest_cost = None
         cheapest_shop = None
-        print(f"{customer.name} has {customer.money} dollars")
         for shop in shops:
-
             trip_cost = customer.get_trip_price(fuel_price, shop)
-
-            print(f"{customer.name}\'s trip to the "
-                  f"{shop.name} costs {trip_cost}")
-
+            print(
+                f"{customer.name}'s trip to the {shop.name} costs {trip_cost}")
             if cheapest_cost is None or trip_cost < cheapest_cost:
                 cheapest_cost = trip_cost
                 cheapest_shop = shop
-
         if customer.money >= cheapest_cost:
             print(f"{customer.name} rides to {cheapest_shop.name}")
             customer.location = cheapest_shop.location
             customer.money -= cheapest_cost
 
-            customer.print_the_purchase_receipt(cheapest_shop)
-            print(f"Total cost is "
-                  f"{customer.get_product_price(cheapest_shop)} dollars")
-            print("See you again!\n")
+            print_purchase_receipt(customer, cheapest_shop)
 
             print(f"{customer.name} rides home")
             print(f"{customer.name} now has {customer.money} dollars\n")
         else:
-            print(f"{customer.name} doesn't have enough money "
-                  f"to make a purchase in any shop")
+            print(f"{customer.name} doesn't have "
+                  f"enough money to make a purchase in any shop")
--- app/shop.py
+++ app/shop.py
@@ -2,11 +2,12 @@
 
 class Shop:
 
-    def __init__(self,
-                 name: str,
-                 shop_location: list[int, int],
-                 products: dict
-                 ) -> None:
+    def __init__(
+            self,
+            name: str,
+            shop_location: list[int, int],
+            products: dict
+    ) -> None:
         self.name = name
         self.location = shop_location
         self.products = products
