7a8c724162711bc7554f38d12d20d2ee40e17b5a
Anastasia <anastasiiya.p@gmail.com>
2023-08-28 18:47:51 +0300

    Fixed_file_path

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from app.calculator import ShopTripCalculator
 
 
 def shop_trip() -> None:
-    trip_calculator = ShopTripCalculator("config.json")
+    trip_calculator = ShopTripCalculator("app/config.json")
     trip_calculator.run()
 
 
