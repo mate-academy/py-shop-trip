47f1b18bb00766367523e6913b00ae008bd52ae2
Bohdan Kolomiiets <kb907406@gmail.com>
2023-09-04 20:13:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,77 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.customer import Customer
+from app.shop import Shop
+from app.car import fuel_price, drive_to_shop, drive_to_home
+
+
+def shop_trip() -> None:
+    with open("config.json") as file:
+        file_info = json.load(file)
+        liter_price = file_info["FUEL_PRICE"]
+        customers_info = file_info["customers"]
+        shops_info = file_info["shops"]
+
+    customers = []
+    shops = []
+
+    for person in customers_info:
+        car = {
+            "brand": person["car"]["brand"],
+            "fuel_consumption": person["car"]["fuel_consumption"]
+        }
+        customers.append(
+            Customer(
+                person["name"],
+                person["product_cart"],
+                person["location"],
+                person["money"],
+                car
+            )
+        )
+
+    for shop in shops_info:
+        shops.append(
+            Shop(
+                shop["name"],
+                shop["location"],
+                shop["products"]
+            )
+        )
+
+    for customer in customers:
+        print(f"{customer.name} has {customer.money}")
+
+        prices_dict = {}
+
+        for store in shops:
+            total_price = round(
+                store.products_price(customer) + fuel_price(
+                    store, customer, liter_price
+                ),
+                2
+            )
+            prices_dict[total_price] = store
+            print(
+                f"{customer.name}'s trip "
+                f"to the {store.name} costs {total_price}"
+            )
+
+        cheapest_price = min(prices_dict)
+        cheapest_store = prices_dict[cheapest_price]
+
+        if not customer.have_enough_money(cheapest_price):
+            print(f"{customer.name} doesn't have enough money"
+                  f" to make a purchase in any shop\n")
+            return
+
+        drive_to_shop(cheapest_store, customer)
+        cheapest_store.print_purchase_receipt(customer)
+        drive_to_home(customer)
+        customer.money -= cheapest_price
+
+        print(f"{customer.name} now has {customer.money} dollars\n")
+
+
+if __name__ == "__main__":
+    shop_trip()
---
+++ app/shop.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from app.customer import Customer
+
+
+class Shop:
+    def __init__(
+            self,
+            name: str,
+            location: list[int],
+            products: dict
+    ) -> None:
+        self.name = name
+        self.location = location
+        self.products = products
+
+    def products_price(self, customer: Customer) -> float | int | str:
+        price = 0
+
+        for name, value in customer.product_cart.items():
+            if name not in self.products:
+                price = -1
+                return f"There is not {name} in {self.name}"
+
+            price += self.products[name] * value
+
+        return price
+
+    def print_purchase_receipt(self, customer: Customer) -> None:
+        today = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        total_price = 0
+
+        print(
+            f"Date: {today}\n"
+            f"Thanks, {customer.name}, for your purchase!\n"
+            "You have bought:"
+        )
+        for product, value in customer.product_cart.items():
+            product_price = value * self.products[product]
+            print(f"{value} {product}s for {product_price} dollars")
+            total_price += product_price
+
+        print(
+            f"Total cost is {total_price} dollars\n"
+            f"See you again\n"
+        )
