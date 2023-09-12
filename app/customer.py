d077d1e888d6cc6099f9ceb533fc1535c3d04270
Olexandr <maxbovling23432@gmail.com>
2023-09-12 22:10:27 +0300

    Merge remote-tracking branch 'origin/master'

--- app/customer.py
+++ app/customer.py
@@ -16,36 +16,9 @@ def customers_and_content() -> list:
     return customers, content, shops, priсe_fuel
 
 
-class CustomerCar:
+class Customer:
     def __init__(
             self,
-            name: str,
-            product_cart: dict,
-            location: list,
-            money: int,
             car: list
     ) -> None:
-        self.name = name
-        self.product_cart = product_cart
-        self.location = location
-        self.money = money
-        self.car = car
-        self.car = Car(**car)
-
-    def customer_location(self) -> dict:
-        fuel_consumption_car = self.car
-        distance_customer_x = self.location[0]
-        distance_customer_y = self.location[1]
-        product_cart = self.product_cart
-        money = self.money
-        name = self.name
-        print(f"{name} has {money} dollars")
-
-        return {
-            "fuel_consumption_car": fuel_consumption_car,
-            "distance_customer_x": distance_customer_x,
-            "distance_customer_y": distance_customer_y,
-            "product_cart": product_cart,
-            "money": money,
-            "name": name,
-        }
+         self.car = Car(**car)
--- app/shop.py
+++ app/shop.py
@@ -4,18 +4,8 @@ from typing import List, Dict
 class Shop:
     def __init__(
             self,
-            name_shop: str,
             location: List[int],
             products: Dict[str, int]
     ) -> None:
-        self.name_shop = name_shop
         self.location = location
         self.products = products
-
-    def shop_location(self) -> dict:
-        return {
-            "name_shop": self.name_shop,
-            "distance_location_shop_x": self.location[0],
-            "distance_location_shop_y": self.location[1],
-            "products": self.products
-        }
