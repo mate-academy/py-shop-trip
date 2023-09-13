d1237f49f9d3e75db108ed0d3b0fad7519f81207
NO19 <5079447@gmil.com>
2023-09-13 13:33:44 +0300

    Solution

--- app/utils/object_creator.py
+++ app/utils/object_creator.py
@@ -3,16 +3,8 @@ from app.shop import Shop
 
 
 def create_customers(customers_data: dict) -> list:
-    customers_objects = []
-    for customer_data in customers_data:
-        customer = Customer(customer_data)
-        customers_objects.append(customer)
-    return customers_objects
+    return [Customer(customer_data) for customer_data in customers_data]
 
 
 def create_shops(shops_data: dict) -> list:
-    shops_objects = []
-    for shop_data in shops_data:
-        shop = Shop(shop_data)
-        shops_objects.append(shop)
-    return shops_objects
+    return [Shop(shop_data) for shop_data in shops_data]
