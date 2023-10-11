005955704db51e507e0171ce5c5e247f212aa88b
Vladyslav Shepilov <shepsterya@gmail.com>
2023-10-11 11:23:08 +0300

    done

--- app/__init__.py
+++ app/__init__.py
@@ -16,14 +16,14 @@ def get_data(file_name: str) -> tuple | None:
         return
 
     if all(key in content for key in ("FUEL_PRICE", "customers", "shops")):
-        customer_list = []
-        shop_list = []
+        customers = []
+        shops = []
         fuel_price = content["FUEL_PRICE"]
 
         for client in content["customers"]:
             if client:
-                customer_list.append(Customer(**client))
+                customers.append(Customer(**client))
         for store in content["shops"]:
             if store:
-                shop_list.append(Shop(**store))
-        return customer_list, shop_list, fuel_price,
+                shops.append(Shop(**store))
+        return customers, shops, fuel_price,
--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ from app import get_data
 
 
 def shop_trip() -> None:
-    customer_list, shop_list, fuel_price = get_data("config.json")
-    for customer in customer_list:
-        cheapest_shop, cost = customer.calculate_trip_cost(shop_list,
+    customers, shops, fuel_price = get_data("config.json")
+    for customer in customers:
+        cheapest_shop, cost = customer.calculate_trip_cost(shops,
                                                            fuel_price)
         customer.trip_to_cheapest(cheapest_shop, cost)
 
