587d25dcb88984dd91779d86f46c6075c1fafefb
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-10-25 22:19:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def shop_trip() -> None:
 
         print(f"Thanks, {client_name}, for your purchase!")
         shops[min_shop_name].process_purchase(client_info)
-        print(f"Total cost is {min_shop_cost - price_of_fuel:.2f} dollars")
+        print(f"Total cost is {min_shop_cost - price_of_fuel:.1f} dollars")
         print("See you again!")
         print("")
 
