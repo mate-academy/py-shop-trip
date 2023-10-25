7cbc81fbc364bf9f01fc42ae3f4dd023828a165e
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-10-25 16:24:29 +0300

    Solution

---
+++ app/customer.py
@@ -0,0 +1,71 @@
+import math
+import datetime
+
+from app.car import Car
+from app.shop import Shop
+
+
+class Customer:
+    def __init__(
+            self,
+            name: str,
+            products: dict,
+            location: list,
+            money: int,
+            car: Car
+    ) -> None:
+        self.name = name
+        self.products = products
+        self.location = location
+        self.money = money
+        self.car = car
+
+    def define_shop(self, shops: list[Shop], fuel_price: float) -> Shop | None:
+
+        print(f"{self.name} has {self.money} dollars")
+
+        cheapest_trip_cost = 0
+        selected_shop: Shop | None = None
+
+        for shop in shops:
+            fuel_cost = (math.dist(self.location, shop.location)
+                         * self.car.fuel_consumption / 100 * fuel_price)
+            products_cost = sum(
+                value * shop.products.get(key)
+                for key, value in self.products.items()
+            )
+            trip_cost = round(fuel_cost * 2 + products_cost, 2)
+            print(f"{self.name}'s trip to the "
+                  f"{shop.name} costs {trip_cost}")
+
+            if not cheapest_trip_cost or trip_cost < cheapest_trip_cost:
+                cheapest_trip_cost = trip_cost
+                selected_shop = shop
+
+        if cheapest_trip_cost > self.money:
+            print(f"{self.name} doesn't have enough money "
+                  f"to make a purchase in any shop")
+            return
+
+        self.money -= cheapest_trip_cost
+        print(f"{self.name} rides to {selected_shop.name}\n")
+        return selected_shop
+
+    def make_purchase(self, selected_shop: Shop) -> None:
+        formatted_date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Date: {formatted_date}")
+        print(f"Thanks, {self.name}, for your purchase!")
+        print("You have bought:")
+
+        total_cost = 0
+        for product, quantity in self.products.items():
+            cost = round(selected_shop.products[product] * quantity, 1)
+            total_cost += cost
+            print(f"{quantity} {product}s for {cost} dollars")
+        print(
+            f"Total cost is {total_cost} dollars")
+        print("See you again!\n")
+
+    def ride_home(self) -> None:
+        print(f"{self.name} rides home")
+        print(f"{self.name} now has {round(self.money, 2)} dollars\n")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,46 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.car import Car
+from app.customer import Customer
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("app/config.json") as config_file:
+        config = json.load(config_file)
+        shops = config["shops"]
+        customers = config["customers"]
+        fuel_price = config["FUEL_PRICE"]
+
+    customers_list = []
+    shops_list = []
+
+    for customer in customers:
+        customer_instance = Customer(
+            name=customer["name"],
+            products=customer["product_cart"],
+            location=customer["location"],
+            money=customer["money"],
+            car=Car(
+                brand=customer["car"]["brand"],
+                fuel_consumption=customer["car"]["fuel_consumption"]
+            )
+        )
+        customers_list.append(customer_instance)
+
+    for shop in shops:
+        shop_instance = Shop(
+            name=shop["name"],
+            location=shop["location"],
+            products=shop["products"]
+        )
+        shops_list.append(shop_instance)
+
+    for customer in customers_list:
+        selected_shop = customer.define_shop(
+            shops=shops_list,
+            fuel_price=fuel_price
+        )
+        if selected_shop:
+            customer.make_purchase(selected_shop=selected_shop)
+            customer.ride_home()
