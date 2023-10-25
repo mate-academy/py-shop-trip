95b94548bfa6ddb8967c540255aa8505c72c0ab0
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2023-10-25 20:50:32 +0300

    Solution

---
+++ app/customer.py
@@ -0,0 +1,49 @@
+from typing import Type
+
+
+class Client:
+    def __init__(
+            self,
+            name: str,
+            product_cart: dict,
+            location: list,
+            money: int,
+            car: dict
+    ) -> None:
+        self.name = name
+        self.product_cart = product_cart
+        self.location = location
+        self.money = money
+        self.car = car
+
+    def calculate_shopping_costs(
+            self,
+            shops: Type,
+            fuel_price: float
+    ) -> None:
+        self.shopping_costs = {}
+        for shop_name, shop in shops.items():
+            total_cost = 0
+            for product, quantity in self.product_cart.items():
+                if product in shop.products:
+                    total_cost += shop.products[product] * quantity
+            total_cost += self.calculate_trip_cost(shop, fuel_price)
+            print(f"{self.name}'s trip to the {shop_name} "
+                  f"Shop costs {total_cost}")
+            self.shopping_costs[shop_name] = total_cost
+
+    def calculate_trip_cost(
+            self,
+            shop: Type,
+            fuel_price: float
+    ) -> float:
+        x1, y1 = self.location
+        x2, y2 = shop.location
+
+        distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+        fuel_consumption = self.car["fuel_consumption"]
+        cost_of_trip = round(
+            (distance / 100) * fuel_consumption * fuel_price * 2, 2
+        )
+
+        return cost_of_trip
---
+++ app/transfer_date.py
@@ -0,0 +1,32 @@
+from typing import IO
+
+from customer import Client
+from shop import Shop
+
+
+def create_clients(data: IO) -> dict:
+    clients = {}
+    for client_data in data["customers"]:
+        name = client_data["name"]
+        client = Client(
+            name=name,
+            product_cart=client_data["product_cart"],
+            location=client_data["location"],
+            money=client_data["money"],
+            car=client_data["car"]
+        )
+        clients[name] = client
+    return clients
+
+
+def create_shops(data: IO) -> dict:
+    shops = {}
+    for shop_data in data["shops"]:
+        name = shop_data["name"]
+        shop = Shop(
+            name=name,
+            location=shop_data["location"],
+            products=shop_data["products"]
+        )
+        shops[name] = shop
+    return shops
