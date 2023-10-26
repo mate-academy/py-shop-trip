de42d47f78507e5c0740d05a2495826619045521
Manynik <s.onufriyenko@gmail.com>
2023-10-26 22:19:58 +0300

    Solution-10

--- app/Shop.py
+++ app/Shop.py
@@ -1,9 +1,10 @@
 class Shop:
-    def __init__(self,
+    def __init__(
+                 self,
                  shop_name: str,
                  shop_location: list,
                  products: dict
-                 ) -> None:
+    ) -> None:
         self.shop_name = shop_name
         self.shop_location = shop_location
         self.products = products
