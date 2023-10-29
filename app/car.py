09268282bcfb7e64d7918cd4befeafc2c85b9bb1
OlehShumov <shumovoleh@ukr.net>
2023-10-29 15:19:48 +0200

    Solution

---
+++ app/car.py
@@ -0,0 +1,18 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class Car:
+    brand: str
+    fuel_consumption: (int, float)
+
+    def calculate_fuel_cost(
+            self,
+            first_point: list,
+            second_point: list,
+            fuel_price: float
+    ) -> float:
+        distance = ((first_point[0] - second_point[0]) ** 2 + (
+                    first_point[1] - second_point[1]) ** 2) ** 0.5
+
+        return (self.fuel_consumption / 100) * distance * fuel_price
