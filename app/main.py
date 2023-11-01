7b2b550e2ea5791f89dd25d0a09e4d8a07cc3ef3
Olexandr Hontarenko <sanya221138@gmail.com>
2023-11-01 14:44:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,49 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.car import Car
+from app.customer import Customer
+from app.shop import Shop
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as conf_file:
+        conf_json_file = json.load(conf_file)
+    fuel_price = conf_json_file["FUEL_PRICE"]
+
+    for item in conf_json_file["customers"]:
+        customer = Customer(
+            name=item["name"],
+            product_cart=item["product_cart"],
+            location=item["location"],
+            money=item["money"]
+        )
+
+        car = Car(
+            fuel_consumption=item["car"]["fuel_consumption"],
+            fuel_price=fuel_price
+        )
+
+        print(f"{customer.name} has {customer.money} dollars")
+        min_price, name_shop = customer.money, ""
+
+        for shop in conf_json_file["shops"]:
+            price_shop = Shop(
+                name=shop["name"],
+                location=shop["location"],
+                cost_products=shop["products"],
+            )
+            price = customer.price_trip_in_shop(car, price_shop)
+            if price < min_price:
+                min_price, name_shop = price, price_shop
+
+        if min_price != customer.money:
+            customer.buy_in_the_shop(min_price, name_shop)
+        else:
+            print(
+                f"{customer.name} "
+                f"doesn't have enough money to make a "
+                f"purchase in any shop"
+            )
+
+
+shop_trip()
