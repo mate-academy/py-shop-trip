a00f990ce0a6f7797d45092357bdcd43b45dc0e7
Ostap <ostapbojko04@gmail.com>
2023-11-08 10:46:22 +0200

    py-shop-trip(1 fixed)

--- app/customer.py
+++ app/customer.py
@@ -1,1 +1,37 @@
-from typing import Dict, Listfrom app.car import Carfrom app.shop import Shopclass Customer:    def __init__(            self,            name: str,            product_cart: List,            location: List,            car: Car,            money: int,    ) -> None:        self.name = name        self.product_cart = product_cart        self.location = location        self.car = car        self.money = money        self.total_cost = None    def calculate_trip_cost(self, shop: Dict, fuel_price: float) -> float:        distance_to_shop = self.calculate_distance(shop.location)        fuel_cost = self.car.calculate_trip_cost(distance_to_shop, fuel_price)        shopping_cost = shop.shopping_cost(self.product_cart)        total_cost = fuel_cost + shopping_cost + fuel_cost        return round(total_cost, 2)    def calculate_distance(self, shop_location: List) -> float:        x1, y1 = self.location        x2, y2 = shop_location        distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5        return distance    def calculate_money(self, shop: Shop) -> bool:        shopping_cost = shop.shopping_cost(self.product_cart)        return self.money >= shopping_cost
+from typing import Dict, List
+from app.car import Car
+from app.shop import Shop
+
+
+class Customer:
+    def __init__(
+            self,
+            name: str,
+            product_cart: List,
+            location: List,
+            car: Car,
+            money: int,
+    ) -> None:
+        self.name = name
+        self.product_cart = product_cart
+        self.location = location
+        self.car = car
+        self.money = money
+        self.total_cost = None
+
+    def calculate_trip_cost(self, shop: Dict, fuel_price: float) -> float:
+        distance_to_shop = self.calculate_distance(shop.location)
+        fuel_cost = self.car.calculate_trip_cost(distance_to_shop, fuel_price)
+        shopping_cost = shop.shopping_cost(self.product_cart)
+        total_cost = fuel_cost + shopping_cost + fuel_cost
+        return round(total_cost, 2)
+
+    def calculate_distance(self, shop_location: List) -> float:
+        x1, y1 = self.location
+        x2, y2 = shop_location
+        distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+        return distance
+
+    def calculate_money(self, shop: Shop) -> bool:
+        shopping_cost = shop.shopping_cost(self.product_cart)
+        return self.money >= shopping_cost
--- app/shop.py
+++ app/shop.py
@@ -1,1 +1,44 @@
-from typing import Dict, Listclass Shop:    def __init__(            self,            name: str,            location: Dict,            product_cart: List,            fuel_price: float    ) -> None:        self.name = name        self.location = location        self.product_cart = product_cart        self.fuel_price = fuel_price    def shopping_cost(self, product_cart: List) -> float:        total_cost = 0        for product in product_cart.keys():            total_cost += self.product_cart[product] * product_cart[product]        return round(total_cost, 2)    def generate_receipt(self, customer: str, current_time: str) -> None:        receipt = f"Date: {current_time}\nThanks, {customer.name}," \                  f" for your purchase!\nYou have bought: \n"        total_cost = 0        for product, quantity in customer.product_cart.items():            if product in self.product_cart:                product_cost = self.product_cart[product]                product_total_cost = product_cost * quantity                if product_total_cost % 1 == 0:                    receipt += (                        f"{quantity} {product}s for "                        f"{int(product_total_cost)} dollars\n"                    )                else:                    receipt += (                        f"{quantity} {product}s for "                        f"{product_total_cost} dollars"                    )                total_cost += product_total_cost        receipt += f"\nTotal cost is {total_cost} dollars\nSee " \                   f"you again!\n\n{customer.name} rides home"        return receipt
+from typing import Dict, List
+
+
+class Shop:
+    def __init__(
+            self,
+            name: str,
+            location: Dict,
+            product_cart: List,
+            fuel_price: float
+    ) -> None:
+        self.name = name
+        self.location = location
+        self.product_cart = product_cart
+        self.fuel_price = fuel_price
+
+    def shopping_cost(self, product_cart: List) -> float:
+        total_cost = 0
+        for product in product_cart.keys():
+            total_cost += self.product_cart[product] * product_cart[product]
+            return round(total_cost, 2)
+
+    def generate_receipt(self, customer: str, current_time: str) -> str:
+        receipt = f"Date: {current_time}\nThanks, {customer.name}," \
+                  f" for your purchase!\nYou have bought: \n"
+        total_cost = 0
+        for product, quantity in customer.product_cart.items():
+            if product in self.product_cart:
+                product_cost = self.product_cart[product]
+                product_total_cost = product_cost * quantity
+                if product_total_cost % 1 == 0:
+                    receipt += (
+                        f"{quantity} {product}s for "
+                        f"{int(product_total_cost)} dollars\n"
+                    )
+                else:
+                    receipt += (
+                        f"{quantity} {product}s for "
+                        f"{product_total_cost} dollars"
+                    )
+                    total_cost += product_total_cost
+                    receipt += f"\nTotal cost is {total_cost} dollars\nSee " \
+                               f"you again!\n\n{customer.name} rides home"
+                    return receipt
