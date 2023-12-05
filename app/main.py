9e2448f7486735acf843ba7fb2e09c9ff85bfad9
Serhii <serhii.domik@gmail.com>
2023-12-05 14:51:51 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,68 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.shop import Shop
+from app.customer import Customer
+from app.car import Car
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "r") as file:
+        info = json.load(file)
+
+        fuel_price = info["FUEL_PRICE"]
+        customers_data = info["customers"]
+        shops_data = info["shops"]
+
+    customers = []
+    for customers_info in customers_data:
+        car = Car(
+            customers_info["car"]["brand"],
+            customers_info["car"]["fuel_consumption"]
+        )
+        customer = Customer(
+            customers_info["name"],
+            customers_info["product_cart"],
+            customers_info["location"],
+            customers_info["money"],
+            car
+        )
+        customers.append(customer)
+
+    shops = []
+    for shop_info in shops_data:
+        shop = Shop(
+            shop_info["name"],
+            shop_info["location"],
+            shop_info["products"]
+        )
+        shops.append(shop)
+
+    for customer in customers:
+
+        print(f"{customer.name} has {customer.money} dollars")
+
+        cheapest_shop = None
+        min_trip_cost = float("inf")
+
+        for shop in shops:
+
+            trip_cost = shop.calculate_trip_cost(
+                customer,
+                fuel_price
+            )
+            print(f"{customer.name}\'s trip to the "
+                  f"{shop.name} costs {round(trip_cost, 2)}")
+
+            if trip_cost < min_trip_cost and trip_cost <= customer.money:
+                min_trip_cost = trip_cost
+                cheapest_shop = shop
+
+        if cheapest_shop:
+            print(f"{customer.name} rides to {cheapest_shop.name}")
+            cheapest_shop.make_purchase(customer)
+            customer.money -= min_trip_cost
+            print(f"{customer.name} rides home")
+            print(f"{customer.name} now has {customer.money:.2f} dollars\n")
+        else:
+            print(f"{customer.name} doesn't have enough money "
+                  f"to make a purchase in any shop")
---
+++ app/shop.py
@@ -0,0 +1,52 @@
+import math
+
+from app.customer import Customer
+
+
+class Shop:
+
+    def __init__(self, name: str, location: list[int], products: dict) -> None:
+        self.name = name
+        self.location = location
+        self.products = products
+
+    def calculate_distance(self, customer: Customer) -> float:
+        return math.sqrt(
+            (self.location[0] - customer.location[0]) ** 2
+            + (self.location[1] - customer.location[1]) ** 2)
+
+    def calculate_trip_cost(
+            self,
+            customer: Customer,
+            fuel_price: float
+    ) -> float:
+        distance_to_shop = self.calculate_distance(customer)
+        fuel_cost_to_shop = ((distance_to_shop / 100)
+                             * customer.car.fuel_consumption * fuel_price)
+        total_product_cost = sum(self.products[item] * count
+                                 for item, count
+                                 in customer.product_cart.items())
+
+        total_cost = fuel_cost_to_shop * 2 + total_product_cost
+
+        return round(total_cost, 2)
+
+    def make_purchase(self, customer: Customer) -> float:
+        total_products_cost = 0
+        receipt = []
+
+        for product, count in customer.product_cart.items():
+            if product in self.products:
+                total_products_cost += count * self.products[product]
+                receipt.append(f"{count} {product}s for "
+                               f"{self.products[product] * count:g} dollars")
+
+        print(f"\nDate: 04/01/2021 12:33:41\n"
+              f"Thanks, {customer.name}, for your purchase!")
+        print("You have bought:")
+        for item in receipt:
+            print(item)
+        print(f"Total cost is {total_products_cost} dollars")
+        print("See you again!\n")
+
+        return total_products_cost
