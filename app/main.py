686423e9f8fd5efb923b915979be9be3ae825bc2
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-29 23:36:50 +0200

    test ok , but FileNotFound

--- app/main.py
+++ app/main.py
@@ -1,69 +1,58 @@
 import json
-from datetime import datetime
+import datetime
+
 from app.customer import Customer
 from app.shop import Shop
 
 
 def shop_trip() -> None:
-    with open("config.json") as file:
-        data = json.load(file)
-        (_, fuel_price), (_, customers), (_, shops) = data.items()
-    customers = [Customer(**customer) for customer in customers]
-    shops = [Shop(**shop) for shop in shops]
+    customers, fuel_price, shops = get_data()
     for customer in customers:
-        print(f"{customer.name} has {customer.money} dollars")
-
-        maxima, choice, bill = float("inf"), None, None
+        head_report = f"{customer.name} has {customer.money} dollars"
+        maxima, choice, bill = float("inf"), 0, 0,
         for shop in shops:
             buy_coast, check = shop.purchase_cost_for(customer)
             if buy_coast:
                 buy_coast = round(buy_coast, 2)
                 trip_coast = buy_coast + customer.car.fuel_cost(
                     customer.distance_to(shop), fuel_price)
-                print(f"{customer.name}'s trip to the "
-                      f"{shop.name} costs {trip_coast}")
-                if maxima > trip_coast:
-                    if trip_coast <= customer.money:
-                        maxima, choice, bill = trip_coast, shop, check
-
-        if choice:
-            date_stamp = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-            print(f"{customer.name} rides to {choice.name}")
-            print()
-            print(f"Date: {date_stamp}")
-            print(f"Thanks, {customer.name}, for your purchase!")
-            print("You have bought")
-            print(bill)
-            print("See you again!")
-            print()
-            print("Bob rides home")
-            print(f"Bob now has {customer.money - maxima} dollars")
-            print()
-        else:
-            print("Monica doesn't have enough money "
-                  "to make a purchase in any shop")
+                head_report += (f"\n{customer.name}'s "
+                                f"trip to the {shop.name} costs {trip_coast}")
+                if maxima > trip_coast <= customer.money:
+                    maxima, choice, bill = trip_coast, shop, check
+        print(head_report)
+        finish_report(bill, choice, customer, maxima)
 
 
-# Bob has 55 dollars
-# Bob's trip to the Outskirts Shop costs 28.21
-# Bob's trip to the Shop '24/7' costs 31.48
-# Bob rides to Outskirts Shop
-#
-# Date: 04/01/2021 12:33:41
-# Thanks, Bob, for your purchase!
-# You have bought:
-# 4 milks for 12 dollars
-# 2 breads for 2 dollars
-# 5 butters for 12.5 dollars
-# Total cost is 26.5 dollars
-# See you again!
-#
-# Bob rides home
-# Bob now has 26.79 dollars
-#
-# Monica has 12 dollars
-# Monica's trip to the Outskirts Shop costs 15.65
-# Monica's trip to the Shop '24/7' costs 16.84
-# Monica doesn't have enough money to make a purchase in any shop
+def finish_report(bill: str,
+                  choice: Shop,
+                  customer: Customer,
+                  maxima: float | int,
+                  ) -> None:
+    if choice:
+        date_stamp = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        customer.money -= maxima
+        print(f"{customer.name} rides to {choice.name}\n"
+              f"\nDate: {date_stamp}"
+              f"\nThanks, {customer.name}, for your purchase!"
+              "\nYou have bought:"
+              f"\n{bill}"
+              f"\nSee you again!\n"
+              f"\n{customer.name} rides home\n"
+              f"{customer.name} now has {customer.money} dollars\n")
+    else:
+        print(f"{customer.name} doesn't have enough money "
+              "to make a purchase in any shop")
+
+
+def get_data() -> tuple[list[Customer], float, list[Shop]]:
+    with open("config.json") as file:
+        data = json.load(file)
+        (_, fuel_price), (_, customers), (_, shops) = data.items()
+    return ([Customer(**customer) for customer in customers],
+            fuel_price,
+            [Shop(**shop) for shop in shops])
+
+
 if __name__ == "__main__":
     shop_trip()
