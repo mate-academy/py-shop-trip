c838761576809f710060c8f19fc6c9a39c049911
Maksym <tarasyukmaxim@gmail.com>
2023-12-31 11:08:16 +0200

    Solution

--- app/customer.py
+++ app/customer.py
@@ -6,6 +6,6 @@ from dataclasses import dataclass
 class Customers:
     name: str
     product_cart: dict
-    location: list
+    location: list[int]
     money: int
     car: dict
--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.customer import Customers
 
 
 def cost_fuel_calculate(
-        customer_location: list,
+        customer_location: list[int],
         shop_location: list,
         fuel_price: float,
         fuel_cons: float
@@ -21,119 +21,94 @@ def cost_fuel_calculate(
     return sum_cost_fuel
 
 
-def cost_food(product_chart: dict, product: dict) -> dict:
-    price_milk = product_chart.get("milk") * product.get("milk")
-    price_bread = product_chart.get("bread") * product.get("bread")
-    price_butter = product_chart.get("butter") * product.get("butter")
-    return {
-        "milk_cost": price_milk,
-        "bread_cost": price_bread,
-        "butter_cost": price_butter}
-
-
 def shop_trip() -> None:
     with open("app/config.json", "r") as file:
         data = json.load(file)
-        fuel_cost = data["FUEL_PRICE"]
-        min_cost = {}
+    fuel_cost = data["FUEL_PRICE"]
+    min_cost = {}
 
-        for customer in data["customers"]:
-            customer = Customers(
-                customer["name"],
-                customer["product_cart"],
-                customer["location"],
-                customer["money"],
-                customer["car"])
-            print(f"{customer.name} has {customer.money} dollars")
+    for customer in data["customers"]:
+        counter = 0
+        total_cost = 0
 
-            customer_home = customer.location
+        customer = Customers(
+            customer["name"],
+            customer["product_cart"],
+            customer["location"],
+            customer["money"],
+            customer["car"])
+        print(f"{customer.name} has {customer.money} dollars")
 
-            for shop in data["shops"]:
-                shops = Shops(
-                    shop["name"],
-                    shop["location"],
-                    shop["products"]
-                )
+        customer_home = customer.location
 
-                fuel = cost_fuel_calculate(
-                    customer_location=customer.location,
-                    shop_location=shops.location,
-                    fuel_price=fuel_cost,
-                    fuel_cons=customer.car["fuel_consumption"]
-                )
+        for shop in data["shops"]:
+            current_shop_food_cost = 0
+            shops = Shops(
+                shop["name"],
+                shop["location"],
+                shop["products"]
+            )
 
-                food_cost = cost_food(customer.product_cart, shops.products)
+            fuel = cost_fuel_calculate(
+                customer_location=customer.location,
+                shop_location=shops.location,
+                fuel_price=fuel_cost,
+                fuel_cons=customer.car["fuel_consumption"]
+            )
 
-                total_cost = (fuel + food_cost.get("milk_cost")
-                              + food_cost.get("bread_cost")
-                              + food_cost.get("butter_cost"))
+            for key in customer.product_cart.keys():
+                current_shop_food_cost += \
+                    customer.product_cart[key] * shops.products[key]
 
-                min_cost.update({shops.name: total_cost})
+            total_cost = fuel + current_shop_food_cost
 
-                print(
-                    f"{customer.name}'s trip to the "
-                    f"{shops.name} costs {total_cost:.2f}")
+            min_cost.update({shops.name: total_cost})
 
-            if customer.money > total_cost:
+            print(
+                f"{customer.name}'s trip to the "
+                f"{shops.name} costs {total_cost:.2f}")
 
-                low_cost_shop_name = min(min_cost, key=min_cost.get)
+        if customer.money > total_cost:
+            low_cost_shop_name = min(min_cost, key=min_cost.get)
 
-                print(f"{customer.name} rides to {low_cost_shop_name}\n")
+            print(f"{customer.name} rides to {low_cost_shop_name}\n")
 
-                customer.location = shops.location
+            customer.location = shops.location
 
-                current_datetime = datetime.datetime.now()
+            current_datetime = datetime.datetime.now()
 
-                print(
-                    f"Date: {current_datetime.strftime('%d/%m/%Y %H:%M:%S')}")
+            print(
+                f"Date: {current_datetime.strftime('%d/%m/%Y %H:%M:%S')}")
 
-                print(f"Thanks, {customer.name}, for your purchase!")
+            print(f"Thanks, {customer.name}, for your purchase!")
 
-                print("You have bought:")
+            print("You have bought:")
 
-                selected_shop_data = next(
-                    shop for shop in data["shops"]
-                    if shop["name"] == low_cost_shop_name)
+            selected_shop_data = next(
+                shop for shop in data["shops"]
+                if shop["name"] == low_cost_shop_name)
 
-                selected_shop = Shops(
-                    selected_shop_data["name"],
-                    selected_shop_data["location"],
-                    selected_shop_data["products"])
-
-                milk = customer.product_cart.get(
-                    "milk") * selected_shop.products.get("milk")
-                bread = customer.product_cart.get(
-                    "bread") * selected_shop.products.get("bread")
-                butter = customer.product_cart.get(
-                    "butter") * selected_shop.products.get("butter")
+            for key, value in selected_shop_data["products"].items():
+                product_cost = customer.product_cart.get(key) * value
+                counter += product_cost
 
                 print(
-                    f"{customer.product_cart.get('milk')} "
-                    f"milks for {milk} dollars"
-                )
+                    f"{customer.product_cart.get(key)} {key}s for "
+                    f"{'%g' % product_cost} dollars")
 
-                print(
-                    f"{customer.product_cart.get('bread')} "
-                    f"breads for {round(bread)} dollars")
+            print(f"Total cost is {counter} dollars")
 
-                print(
-                    f"{customer.product_cart.get('butter')} "
-                    f"butters for {butter} dollars")
+            print("See you again!\n")
 
-                print(f"Total cost is {milk + bread + butter} dollars")
+            print(f"{customer.name} rides home")
 
-                print("See you again!\n")
+            customer.location = customer_home
 
-                print(f"{customer.name} rides home")
+            print(f"{customer.name} now has "
+                  f"{customer.money - min_cost.get(selected_shop_data.get('name')):.2f}"
+                  f" dollars\n")
 
-                customer.location = customer_home
-
-                print(
-                    f"{customer.name} now has "
-                    f"{customer.money - min_cost.get(selected_shop.name):.2f} "
-                    f"dollars\n")
-
-            else:
-                print(
-                    f"{customer.name} doesn't have enough money "
-                    f"to make a purchase in any shop")
+        else:
+            print(
+                f"{customer.name} doesn't have enough money "
+                f"to make a purchase in any shop")
