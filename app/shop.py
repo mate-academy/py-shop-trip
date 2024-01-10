97587aafcd2f6b8815f79961bb365c477730032c
2ilga <2lgan1601@gmail.com>
2024-01-10 17:49:18 +0200

    it works!2

--- app/shop.py
+++ app/shop.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 
@@ -29,8 +30,11 @@ class Shop:
             if product in self.products:
                 product_price = self.products[product]
                 cost_for_product = product_price * quantity
-                if cost_for_product.is_integer():
-                    cost_for_product = int(cost_for_product)
+                cost_for_product_frac, cost_for_product_int \
+                    = math.modf(cost_for_product)
+                cost_for_product_after = cost_for_product_frac * 10
+                if cost_for_product_after == 0:
+                    cost_for_product = int(cost_for_product_int)
                 print(f"{quantity} {product}s for {cost_for_product} dollars")
         total_cost = self.calculate_purchase_cost(customer)
         print(f"Total cost is {total_cost} dollars")
