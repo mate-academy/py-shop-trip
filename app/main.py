c039ce291aeafdb66d969cd6d8fc20421255932e
Viktor Rura <ruravictor@gmail.com>
2024-01-18 18:41:35 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -5,6 +5,13 @@ from .shop import Shop
 from .data_base import DataBase
 
 
+def format_price(num: int | float) -> int | float:
+    if float(num).is_integer():
+        return int(num)
+
+    return num
+
+
 def shop_trip() -> None:
     data = DataBase()
     customers = []
@@ -33,17 +40,17 @@ def shop_trip() -> None:
     for customer in customers:
         print(f"{customer.name} has {customer.money} dollars")
 
-        for _shop in shops:
+        for shop in shops:
             shop_price = (
-                (((_shop.get_distance(customer.location) / 100) * customer.car[
+                (((shop.get_distance(customer.location) / 100) * customer.car[
                     "fuel_consumption"]) * fuel_price) * 2 +
-                + _shop.products_cost(customer.product_cart)
+                + shop.products_cost(customer.product_cart)
             )
             shop_price = Decimal(shop_price).quantize(Decimal("0.00"))
-            trip_cost[shop_price] = _shop
+            trip_cost[shop_price] = shop
             print(
                 f"{customer.name}'s trip to "
-                f"the {_shop.name} costs {shop_price}"
+                f"the {shop.name} costs {shop_price}"
             )
 
         money_need = min(trip_cost)
@@ -53,53 +60,41 @@ def shop_trip() -> None:
                 f"{customer.name} doesn't have enough money "
                 f"to make a purchase in any shop"
             )
+            break
 
-        else:
-            money_need = min(trip_cost)
-            shop_need = trip_cost[money_need]
+        money_need = min(trip_cost)
+        shop_need = trip_cost[money_need]
 
-            print(f"{customer.name} rides to {shop_need.name}\n")
-            print(
-                f"Date: "
-                f"{datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
+        print(f"{customer.name} rides to {shop_need.name}\n")
+        print(
+            f"Date: "
+            f"{datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
+        )
+
+        print(
+            f"Thanks, {customer.name}, for your purchase!\n"
+            f"You have bought:"
+        )
+
+        money_spent = 0
+
+        for product, quantity in customer.product_cart.items():
+            product_cost = (
+                customer.product_cart[product] * shop_need.products[product]
             )
-            milk_quantity = customer.product_cart["milk"]
-            milk_cost = (
-                customer.product_cart["milk"] * shop_need.products["milk"]
-            )
-
-            bread_quantity = customer.product_cart["bread"]
-            bread_cost = (
-                customer.product_cart["bread"] * shop_need.products["bread"]
-            )
-
-            butter_quantity = customer.product_cart["butter"]
-            butter_cost = (
-                customer.product_cart["butter"] * shop_need.products["butter"]
-            )
-
-            def format_price(num: int | float) -> int | float:
-                if float(num).is_integer():
-                    return int(num)
-                else:
-                    return num
+            money_spent += product_cost
+            product_quantity = quantity
 
             print(
-                f"Thanks, {customer.name}, for your purchase!\n"
-                f"You have bought:\n"
-                f"{milk_quantity} milks "
-                f"for {format_price(milk_cost)} dollars\n"
-                f"{bread_quantity} breads "
-                f"for {format_price(bread_cost)} dollars\n"
-                f"{butter_quantity} butters "
-                f"for {format_price(butter_cost)} dollars\n"
-                f"Total cost is "
-                f"{shop_need.products_cost(customer.product_cart)} dollars\n"
-                f"See you again!\n"
+                f"{product_quantity} {product}s "
+                f"for {format_price(product_cost)} dollars"
             )
+        money_left = customer.money - money_need
 
-            print(f"{customer.name} rides home")
-            print(
-                f"{customer.name} now "
-                f"has {customer.money - money_need} dollars\n"
-            )
+        print(
+            f"Total cost is {money_spent} dollars\n"
+            f"See you again!\n"
+            f"\n"
+            f"{customer.name} rides home\n"
+            f"{customer.name} now has {money_left} dollars\n"
+        )
