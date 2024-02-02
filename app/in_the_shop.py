cf55b6b866da32a888f8015e9e4baa99aed1361e
Maksym Hovorunenko <govorunenko@gmail.com>
2024-02-02 15:22:23 +0200

    Solution

--- app/in_the_shop.py
+++ app/in_the_shop.py
@@ -6,7 +6,9 @@ from app.shop import Shop
 def shopping_in_the_store(customer: Customer, shop: Shop) -> None:
     print("Date: 04/01/2021 12:33:41")
     print(f"Thanks, {customer.name}, for your purchase!")
-    # keys_only = list(customer.product_cart.keys())
+    amount_of_milk = customer.product_cart["milk"]
+    amount_of_bread = customer.product_cart["bread"]
+    amount_of_butter = customer.product_cart["butter"]
     milk_cost = Decimal(str(customer.product_cart["milk"]
                             * shop.products["milk"]))
     bread_cost = Decimal(str(int(customer.product_cart["bread"]
@@ -14,9 +16,9 @@ def shopping_in_the_store(customer: Customer, shop: Shop) -> None:
     butter_cost = Decimal(str(customer.product_cart["butter"]
                               * shop.products["butter"]))
     print("You have bought:")
-    print(f"{customer.product_cart["milk"]} milks for {milk_cost} dollars")
-    print(f"{customer.product_cart["bread"]} breads for {bread_cost} dollars")
-    print(f"{customer.product_cart["butter"]} butters for {butter_cost}"
+    print(f"{amount_of_milk} milks for {milk_cost} dollars")
+    print(f"{amount_of_bread} breads for {bread_cost} dollars")
+    print(f"{amount_of_butter} butters for {butter_cost}"
           f" dollars")
     print(f"Total cost is {milk_cost + bread_cost + butter_cost} dollars")
     print("See you again!")
