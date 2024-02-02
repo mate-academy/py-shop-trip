a53489487f2b50daeb508117abba6655f3c81b5a
Maksym Hovorunenko <govorunenko@gmail.com>
2024-02-02 15:12:55 +0200

    Solution

--- app/in_the_shop.py
+++ app/in_the_shop.py
@@ -15,11 +15,11 @@ def shopping_in_the_store(customer: Customer, shop: Shop) -> None:
                               * shop.products["butter"]))
     print("You have bought:")
     print(f"{customer.product_cart["milk"]} {keys_only[0]}s"
-          f" for {milk_cost} dollars\n"
-          f"{customer.product_cart["bread"]} {keys_only[1]}s"
-          f" for {bread_cost} dollars\n"
-          f"{customer.product_cart["butter"]} {keys_only[2]}s"
-          f" for {butter_cost} dollars\n"
-          f"Total cost is {milk_cost + bread_cost + butter_cost} dollars\n"
-          f"See you again!\n"
-          f"")
+          f" for {milk_cost} dollars")
+    print(f"{customer.product_cart["bread"]} {keys_only[1]}s"
+          f" for {bread_cost} dollars")
+    print(f"{customer.product_cart["butter"]} {keys_only[2]}s"
+          f" for {butter_cost} dollars")
+    print(f"Total cost is {milk_cost + bread_cost + butter_cost} dollars\n"
+          f"See you again!")
+    print("")
