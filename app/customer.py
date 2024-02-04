2c0cb702829e190a7fd8264f278018d3746f0838
Sikach Illia <sikatch.ilja@gmail.com>
2024-02-04 19:15:58 +0200

    Solution

---
+++ app/customer.py
@@ -0,0 +1,79 @@
+import datetime
+
+from app.parse_all import ParseJsonMixin
+from app.shop import Shop
+from app.car import Car
+
+
+class Customer(ParseJsonMixin):
+    customers = []
+
+    def __init__(
+            self,
+            name: str = None,
+            product_cart: dict = None,
+            location: list[int] = None,
+            money: int = None,
+            car: Car = None,
+    ) -> None:
+        self.name = name
+        self.product_cart = product_cart
+        self.location = location
+        self.money = money
+        self.car = Car(**car)
+        self.full_cost = {}
+        self.chosen_shop = None
+        Customer.customers.append(self)
+
+    def calculate_costs_shopping_per_shop(self) -> None:
+        for shop in Shop.all_shops:
+            self.full_cost[shop.name] = 0
+            for product in self.product_cart.keys():
+                self.full_cost[shop.name] += (shop.products[product]
+                                              * self.product_cart[product])
+            way_to_shop = self.car.cost_of_way(shop.location, self.location)
+            self.full_cost[shop.name] += way_to_shop
+            print(f"{self.name}'s trip to the {shop.name} "
+                  f"costs {self.full_cost[shop.name]}")
+
+    def choose_cheapest_shop(self) -> None:
+        lowest_cost = min(self.full_cost.values())
+        cheapest = {
+            key: value
+            for key, value
+            in self.full_cost.items()
+            if value == lowest_cost
+        }
+        if list(cheapest.values())[0] < self.money:
+            for shop in Shop.all_shops:
+                if shop.name == list(cheapest.keys())[0]:
+                    self.chosen_shop = shop
+            print(f"{self.name} rides to {self.chosen_shop.name}\n")
+
+    def bill_shipping(self) -> None:
+        now = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        if not self.chosen_shop:
+            print(f"{self.name} doesn't have enough "
+                  f"money to make a purchase in any shop")
+            return
+
+        print(f"Date: {now}\n"
+              f"Thanks, {self.name}, for your purchase!\n"
+              f"You have bought:")
+
+        total_cost = 0
+        for name in self.product_cart.keys():
+            cost = self.chosen_shop.products[name] * self.product_cart[name]
+            if cost == int(cost):
+                cost = int(cost)
+            print(f"{self.product_cart[name]} {name}s "
+                  f"for {cost} dollars")
+            total_cost += cost
+
+        print(f"Total cost is {total_cost} dollars\nSee you again!\n")
+
+        print(f"{self.name} rides home")
+
+        print(f"{self.name} now has "
+              f"{self.money - self.full_cost[self.chosen_shop.name]} "
+              f"dollars\n")
--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-def shop_trip():
-    # write your code here
-    pass
+import os
+
+from app.shop import Shop
+from app.customer import Customer
+from app.car import Car
+
+
+def shop_trip() -> None:
+    app_folder_path = os.path.dirname(os.path.abspath(__file__))
+    file_name = "config.json"
+    config_json = os.path.join(app_folder_path, file_name)
+    Shop.parse_data_from_json("shops", config_json)
+    Customer.parse_data_from_json("customers", config_json)
+    Car.parse_data_from_json("fuel", config_json)
+    for person in Customer.customers:
+        print(f"{person.name} has {person.money} dollars")
+        person.calculate_costs_shopping_per_shop()
+        person.choose_cheapest_shop()
+        person.bill_shipping()
