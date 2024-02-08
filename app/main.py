82787e08a75b346c01e351110d11b137b7d018df
Yura Push <youdeimos1@gmail.com>
2024-02-08 21:06:31 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,3 +1,37 @@
-def shop_trip():
-    # write your code here
-    pass
+import json
+
+from app.customer import Customer
+from app.shop import Shop
+from app.show_information import show_trip_info
+
+
+def shop_trip() -> None:
+    with open("app/config.json", "rb") as data_file:
+        data = json.load(data_file)
+
+    fuel_price = data["FUEL_PRICE"]
+
+    customers_instances = [
+        Customer(
+            customer["name"],
+            customer["product_cart"],
+            customer["location"],
+            customer["money"],
+            customer["car"]
+        )
+        for customer in data["customers"]
+    ]
+    shops_instances = [
+        Shop(
+            shop["name"],
+            shop["location"],
+            shop["products"]
+        )
+        for shop in data["shops"]
+    ]
+
+    for customer in customers_instances:
+        customer.calculate_total_expenses(shops_instances, fuel_price)
+        customer.choose_shop_with_minimal_expenses()
+        for shop, value in customer.best_shop.items():
+            show_trip_info(customer, value)
