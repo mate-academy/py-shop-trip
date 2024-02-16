47effe60f539a11b50f848cf5436094943420fc0
Юрий <you@exampleabc1234567892777@gmail.com>
2024-02-16 13:46:47 +0200

    'Solution3'

--- app/customer.py
+++ app/customer.py
@@ -1,8 +1,16 @@
 from math import sqrt
 import datetime
+from typing import Dict, Any
+
+
 class Customer:
 
-    def __init__(self, name, product_cart, location, money, car):
+    def __init__(
+            self, name: str,
+            product_cart: Dict[str, Any],
+            location: tuple,
+            money: float, car: Any
+    ) -> None:
         self.name = name
         self.product_cart = product_cart
         self.location = location
@@ -10,30 +18,30 @@ class Customer:
         self.car = car
 
     @staticmethod
-    def calculate_distance(location1, location2):
-        distance = sqrt((location1[0] - location2[0]) ** 2 + (location1[1] - location2[1]) ** 2)
-        return round(distance, 2)
-        # return distance
+    def calculate_distance(location1: tuple, location2: tuple) -> float:
+        delta_x = location1[0] - location2[0]
+        delta_y = location1[1] - location2[1]
+        distance = sqrt(delta_x ** 2 + delta_y ** 2)
+        return distance
 
-    def purchase_receipt(self, shop_name, products):
-        # current_time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+    def purchase_receipt(
+            self, shop_name: str,
+            products: Dict[str, float]
+    ) -> None:
         current_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-        print(f"\nDate: {current_time}\nThanks, {self.name}, for your purchase!\nYou have bought:")
-        # print(f"\nDate: {current_time}\nThanks, {self.name}, for your purchase at {shop_name}!\nYou have bought:")
-
+        print(
+            f"\nDate: {current_time}\nThanks, "
+            f"{self.name}, "
+            f"for your purchase!\nYou have bought:"
+        )
 
         total_cost = 0
         for item, quantity in self.product_cart.items():
-            # cost = products[item] * quantity
-            # total_cost += round(cost, 2)
-            # print(f"{quantity} {item}s for {round(cost, 2)} dollars")
+
             cost = products[item] * quantity
             total_cost += cost
             cost = int(cost) if cost == int(cost) else round(cost, 1)
             print(f"{quantity} {item}s for {cost} dollars")
 
-        # print(f"Total cost is {total_cost:.2f} dollars\nSee you again!")
-        # print(f"Total cost is {round(total_cost, 2)} dollars\nSee you again!")
-        print(f"Total cost is {total_cost:.2f} dollars\nSee you again!\n")
-
+        print(f"Total cost is {total_cost:.1f} dollars\nSee you again!\n")
--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 import json
 import os
-from math import isclose
-
 from app.car import Car
 from app.customer import Customer
 from app.shop import Shop
 
 
-def shop_trip():
+def shop_trip() -> None:
     with open(os.path.join("app", "config.json")) as config_file:
         config_data = json.load(config_file)
 
@@ -28,39 +26,66 @@ def shop_trip():
 
     shops = []
     for shop_data in config_data["shops"]:
-        shop = Shop(shop_data["name"], shop_data["location"], shop_data["products"])
+        shop = Shop(
+            shop_data["name"],
+            shop_data["location"],
+            shop_data["products"]
+        )
         shops.append(shop)
 
     for customer in customers:
-        # print(f"\n{customer.name} has {customer.money} dollars")
         print(f"{customer.name} has {customer.money} dollars")
-        min_trip_cost = float('inf')
+        min_trip_cost = float("inf")
         selected_shop = None
 
         for shop in shops:
-            distance_to_shop = Customer.calculate_distance(customer.location, shop.location)
-            fuel_cost_to_shop = (distance_to_shop / 100) * customer.car.fuel_consumption * fuel_price
-            product_cost = sum(customer.product_cart[item] * shop.products[item] for item in customer.product_cart)
-            fuel_cost_to_home = (distance_to_shop / 100) * customer.car.fuel_consumption * fuel_price
+            distance_to_shop = Customer.calculate_distance(
+                customer.location,
+                shop.location
+            )
+            fuel_cost_to_shop = distance_to_shop / 100
+            fuel_cost_to_shop *= customer.car.fuel_consumption
+            fuel_cost_to_shop *= fuel_price
 
-            total_trip_cost = fuel_cost_to_shop + product_cost + fuel_cost_to_home
-            print(f"{customer.name}'s trip to the {shop.name} costs {round(total_trip_cost, 2)}")
+            product_cost = sum(
+                customer.product_cart[item]
+                * shop.products[item] for item in customer.product_cart
+            )
+            fuel_cost_to_home = distance_to_shop / 100
+            fuel_cost_to_home *= customer.car.fuel_consumption
+            fuel_cost_to_home *= fuel_price
+            total_trip_cost = fuel_cost_to_shop
+            total_trip_cost += product_cost
+            total_trip_cost += fuel_cost_to_home
 
-            # if isclose(total_trip_cost, min_trip_cost) and total_trip_cost <= customer.money:
-            if total_trip_cost < min_trip_cost and total_trip_cost <= customer.money:
+            print(
+                f"{customer.name}'s trip to the "
+                f"{shop.name} costs "
+                f"{round(total_trip_cost, 2)}"
+            )
+
+            if (
+                    total_trip_cost < min_trip_cost
+            ) and (
+                    total_trip_cost <= customer.money
+            ):
                 min_trip_cost = total_trip_cost
                 selected_shop = shop
 
         if selected_shop:
             print(f"{customer.name} rides to {selected_shop.name}")
-            customer.purchase_receipt(selected_shop.name, selected_shop.products)
+            customer.purchase_receipt(
+                selected_shop.name, selected_shop.products
+            )
             print(f"{customer.name} rides home")
             customer.money -= min_trip_cost
-            # print(f"{customer.name} now has {round(customer.money, 2)} dollars")
-            print(f"{customer.name} now has {round(customer.money, 2)} dollars\n")
+            print(f"{customer.name} "
+                  f"now has "
+                  f"{round(customer.money, 2)} dollars\n")
 
         else:
-            print(f"{customer.name} doesn't have enough money to make a purchase in any shop")
+            print(f"{customer.name} "
+                  f"doesn't have enough money to make a purchase in any shop")
 
 
 if __name__ == "__main__":
