b49a94fb93fe2e5ee4f8acd7fd0f304e228d0579
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-02-20 22:45:13 +0100

    fix flake

--- app/customer_trip.py
+++ app/customer_trip.py
@@ -19,31 +19,35 @@ class Trip:
 
     @staticmethod
     def print_date(customer: dict) -> None:
+        name = customer["name"]
         date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        print(f"\nDate: {date}\nThanks, {customer["name"]}, "
+        print(f"\nDate: {date}\nThanks, {name}, "
               f"for your purchase!\nYou have bought:")
 
     def check_perf_purchase(self, customer: dict,
                             shop_data: tuple,
                             ) -> bool:
+        name = customer["name"]
         shop_index = shop_data[1][1]
-        if (customer["money"] < shop_data[1][1]
+        if (customer["money"] < shop_data[1][0]
                 + Shop.check_money(
                     customer["product_cart"],
-                    self.data["shops"][shop_data[1][1]]["products"])):
-            print(f"{customer["name"]} doesn't have enough money to make a"
+                    self.data["shops"][shop_index]["products"])):
+            print(f"{name} doesn't have enough money to make a"
                   f" purchase in any shop")
             return False
         else:
-            shop_name = self.data["shops"][shop_data[1][1]]["name"]
-            print(f"{customer["name"]} rides to {shop_name}")
+            shop_name = self.data["shops"][shop_index]["name"]
+            print(f"{name} rides to {shop_name}")
             customer["location"] = self.data["shops"][shop_index]["location"]
             return True
 
     def customer_trip(self) -> None:
         # Loop of customers data.
         for customer in self.data["customers"]:
-            print(f"""{customer["name"]} has {customer["money"]} dollars""")
+            name = customer["name"]
+            money = customer["money"]
+            print(f"""{name} has {money} dollars""")
 
             # Loop of shop data.
             shop_data = Shop(self.data,
