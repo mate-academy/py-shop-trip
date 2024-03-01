e025127da5edbc06083487b8efa24a17400444ec
Anna Zadorozhna <annalepiska@gmail.com>
2024-03-01 16:09:56 +0200

    Solution

--- app/cost_calculation.py
+++ app/cost_calculation.py
@@ -16,7 +16,7 @@ def get_fuel_cost(
     fuel_cost = (fuel_price * distance
                  * customer.car.fuel_consumption / 100)
 
-    return round(fuel_cost, 2)
+    return fuel_cost
 
 
 def get_product_cost(
@@ -30,24 +30,24 @@ def get_product_cost(
         products_cost = number * shop.product_cart[product]
         total_products_cost += products_cost
 
-    return round(total_products_cost, 2)
+    return total_products_cost
 
 
 def get_receipt(
         customer: Customer,
         shop: Shop
 ) -> None:
-    print((f"Date: {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n"
-           f"Thanks, {customer.name}, for your purchase!\n"
-           f"You have bought:"))
+    print(f"Date: {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n"
+          f"Thanks, {customer.name}, for your purchase!\n"
+          f"You have bought:")
 
     for product, number in customer.product_cart.items():
-        products_cost = number * shop.product_cart[product]
+        products_cost = round(number * shop.product_cart[product], 2)
         if products_cost % 1 == 0:
             products_cost = int(products_cost)
         print(f"{number} {product}s for {products_cost} dollars")
 
     total_product_cost = get_product_cost(customer, shop)
-    print(f"Total cost is {total_product_cost} dollars\n"
+    print(f"Total cost is {round(total_product_cost, 2)} dollars\n"
           f"See you again!\n\n"
           f"{customer.name} rides home")
